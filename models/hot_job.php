afbab54a1a3df00caa04e9cc2e8201ae9309a8ab
Brandon Cordell <brandonc@ctsfla.com>
2011-02-28 09:26:26 -0500

    fixed typos in the hot_job_fixture, and created tests for the HotJob model and made them pass

--- models/hot_job.php
+++ models/hot_job.php
@@ -2,5 +2,26 @@
 class HotJob extends AppModel {
 	var $name = 'HotJob';
 	var $displayField = 'title';
+	
+	var $validate = array(
+		'employer' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must include an employer or check not specified'
+			)
+		),
+		'title' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a title for this job'
+			)
+		),
+		'description' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a description for this job'
+			)
+		)
+	);
 }
 ?>
--- tests/cases/models/hot_job.test.php
+++ tests/cases/models/hot_job.test.php
@@ -14,5 +14,51 @@ class HotJobTestCase extends AtlasTestCase {
 		ClassRegistry::flush();
 	}
 
+	function testValidation() {
+		$this->HotJob->create();
+		
+		$invalidRecordNoEmployer = array(
+			'HotJob' => array(
+				'employer' => '',
+				'title' => 'CNC Swiss Lathe 7 Axis Operator',
+				'description' => 'Must have HS/GED w/5 yrs exp in CNC lathe machinery & familiar w/ISO 9001 requirements. Will set-up, program & operate 2 CNC Swiss CNC lathe 7 axis machines. Pay: $15-25/hr.',
+				'location' => 'Pinellas County',
+				'url' => 'http://cncists.com',
+				'reference_number' => '9509835',
+				'contact' => 'Email resume in Word wit Ref # to hloeun@worknetpinellas.org',
+				'file' => ''
+			)
+		);
+		
+		$invalidRecordNoTitle = array(
+			'HotJob' => array(
+				'employer' => 'CNCists',
+				'title' => '',
+				'description' => 'Must have HS/GED w/5 yrs exp in CNC lathe machinery & familiar w/ISO 9001 requirements. Will set-up, program & operate 2 CNC Swiss CNC lathe 7 axis machines. Pay: $15-25/hr.',
+				'location' => 'Pinellas County',
+				'url' => 'http://cncists.com',
+				'reference_number' => '9509835',
+				'contact' => 'Email resume in Word wit Ref # to hloeun@worknetpinellas.org',
+				'file' => ''
+			)
+		);
+		
+		$invalidRecordNoDescription = array(
+			'HotJob' => array(
+				'employer' => 'CNCists',
+				'title' => 'CNC Swiss Lathe 7 Axis Operator',
+				'description' => '',
+				'location' => 'Pinellas County',
+				'url' => 'http://cncists.com',
+				'reference_number' => '9509835',
+				'contact' => 'Email resume in Word wit Ref # to hloeun@worknetpinellas.org',
+				'file' => ''
+			)
+		);
+		
+		$this->assertFalse($this->HotJob->save($invalidRecordNoEmployer));
+		$this->assertFalse($this->HotJob->save($invalidRecordNoTitle));
+		$this->assertFalse($this->HotJob->save($invalidRecordNoDescription));
+	}
 }
 ?>
