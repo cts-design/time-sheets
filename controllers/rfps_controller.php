2f63bbb955b0fa6a9b879949530f09c41ae39279
Brandon Cordell <brandonc@ctsfla.com>
2011-03-01 08:50:18 -0500

    Added transactions, the stubbed admin_data_delegate, and added an uploadFile function

--- controllers/rfps_controller.php
+++ controllers/rfps_controller.php
@@ -12,11 +12,26 @@ class RfpsController extends AppController {
 		$this->Rfp->recursive = 0;
 		$this->set('rfps', $this->paginate());
 	}
+	
+	function admin_data_delegate() {
+		if($this->RequestHandler->isAjax()) {
+			$params = $this->params['form'];
+			
+			FireCake::log($this->params);
+			
+			switch ($params['xaction']) {
+				case 'read':
+					break;
+			}
+	    }		
+	}
 
 	function admin_add() {
 		if (!empty($this->data)) {
 			$this->Rfp->create();
 			if ($this->Rfp->save($this->data)) {
+				$this->Rfp->createUserTransaction('CMS', null, null,
+                                        'Created RFP ID ' . $this->Rfp->id);
 				$this->Session->setFlash(__('The rfp has been saved', true), 'flash_success');
 				$this->redirect(array('action' => 'index'));
 			} else {
@@ -32,6 +47,8 @@ class RfpsController extends AppController {
 		}
 		if (!empty($this->data)) {
 			if ($this->Rfp->save($this->data)) {
+				$this->Rfp->createUserTransaction('CMS', null, null,
+                                        'Edited RFP ID ' . $id);
 				$this->Session->setFlash(__('The rfp has been saved', true), 'flash_success');
 				$this->redirect(array('action' => 'index'));
 			} else {
@@ -49,11 +66,51 @@ class RfpsController extends AppController {
 			$this->redirect(array('action'=>'index'));
 		}
 		if ($this->Rfp->delete($id)) {
+			$this->Rfp->createUserTransaction('CMS', null, null,
+                                        'Deleted RFP ID ' . $id);
 			$this->Session->setFlash(__('Rfp deleted', true), 'flash_success');
 			$this->redirect(array('action'=>'index'));
 		}
 		$this->Session->setFlash(__('Rfp was not deleted', true), 'flash_failure');
 		$this->redirect(array('action' => 'index'));
 	}
+	
+	function _uploadFile() {
+        // get the document relative path to the inital storage folder
+        $abs_path = WWW_ROOT . 'files/public/rfps/';
+        $rel_path = 'files/public/rfps/';
+        $file_ext = '';
+        $filename = '';
+
+        switch($this->data['Rfp']['file']['type']) {
+            case 'application/pdf':
+                $file_ext = '.pdf';
+                break;
+            case 'application/msword':
+                $file_ext = '.doc';
+                break;
+            case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                $file_ext = '.docx';
+                break;
+        }
+		
+        $filename = date('YmdHis') . $file_ext;
+
+        // check to see if the directory exists
+        if (!is_dir($abs_path)) {
+            mkdir($abs_path);
+        }
+
+        if (!file_exists($abs_path . $filename)) {
+            $full_url = $abs_path . $filename;
+            $url = $rel_path . $filename;
+
+            if (!move_uploaded_file($this->data['Rfp']['file']['tmp_name'], $url)) {
+                return false;
+            }
+        }
+
+        return $url;
+    }
 }
 ?>
