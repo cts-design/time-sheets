ae4661e11c75c001bed708589e4ccfc42c7e4a06
Daniel Nolan <daniel@ctsfla.com>
2011-03-03 16:15:07 -0500

    commented out symlink lines

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,10 @@ set :deploy_via, :export
 
 ssh_options[:forward_agent] = true
 
-
 set :deploy_to, "/var/www/vhosts/tbwa.ctsfla.com/atlas"
 
 server "tbwa.ctsfla.com", :app, :web, :db, :primary => true
-default_environment['PATH'] = "/var/www/vhosts/tbwa.ctsfla.com/atlas/shared/cakephp/cake/console:$PATH"
+#default_environment['PATH'] = "/var/www/vhosts/tbwa.ctsfla.com/atlas/shared/cakephp/cake/console:$PATH"
 default_run_options[:pty] = true
 
 set :user, 'tbwaftp'
@@ -22,23 +21,24 @@ set :user, 'tbwaftp'
 set :cake_branch, "master"
 
 
-	desc "Update database schema create tables"
-		task :migrate_database_create, roles => [:web] do
-		run "cd #{current_release} && cake schema create atlas < #{shared_path}/config/schema_create_prompt.txt"
-	end
-	
-	desc "Update database schema update tables"
-	task :migrate_database_update, roles => [:web] do
-		run "cd #{current_release} && yes y | cake schema update atlas"
-	end  
-	
-	task :finalize_deploy, :roles => [:web] do
-		run "chmod 755 -R #{release_path}"
-		run "mv #{release_path}/webroot/index.staging.php #{release_path}/webroot/index.php"
-		run "ln -s #{shared_path}/system #{release_path}/webroot/"
-		run "mv #{release_path}/config/atlas.default.php #{release_path}/config/atlas.php"
-		run "mv #{release_path}/config/core.default.php #{release_path}/config/core.php"
-	end	
+desc "Update database schema create tables"
+	task :migrate_database_create, roles => [:web] do
+	run "cd #{current_release} && cake schema create atlas < #{shared_path}/config/schema_create_prompt.txt"
+end
+
+desc "Update database schema update tables"
+task :migrate_database_update, roles => [:web] do
+	run "cd #{current_release} && yes y | cake schema update atlas"
+end  
+
+task :finalize_deploy, :roles => [:web] do
+	run "chmod 755 -R #{release_path}"
+	run "mv #{release_path}/webroot/index.staging.php #{release_path}/webroot/index.php"
+	#run "ln -s #{shared_path}/system #{current_release}/webroot/"
+	#run "ln -s #{shared_path}/storage #{current_release}/storage"
+	run "mv #{release_path}/config/atlas.default.php #{release_path}/config/atlas.php"
+	run "mv #{release_path}/config/core.default.php #{release_path}/config/core.php"
+end	
 
 after "deploy:update_code", :finalize_deploy
 
