a18c23c1eaa12dc2c82761509c8c380090763573
Daniel Nolan <daniel@ctsfla.com>
2011-03-03 15:23:18 -0500

    updated twba staging deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,26 +21,25 @@ set :user, 'tbwaftp'
 # Cake Settings
 set :cake_branch, "master"
 
-task :finalize_deploy, :roles => [:web] do
-	run "chmod 755 -R #{release_path}"
-	run "mv #{release_path}/webroot/index_staging.php #{release_path}/webroot/index.php"
-	run "ln -fs #{current_path}/webroot ~/public_html"
-	#run "ln -s #{shared_path}/system ~/public_html/system"
-	run "mv #{release_path}/config/atlas.default.php #{release_path}/config/atlas.php"
-	run "ln -s #{shared_path}/plugins #{current_release}/plugins"
-	run "ln -s #{shared_path}/config/core.php #{current_release}/config/core.php"
-	run "ln -s #{shared_path}/storage #{current_release}/storage"
-end
 
-desc "Update database schema create tables"
-	task :migrate_database_create, roles => [:web] do
-	run "cd #{current_release} && cake schema create atlas < #{shared_path}/config/schema_create_prompt.txt"
-end
+	desc "Update database schema create tables"
+		task :migrate_database_create, roles => [:web] do
+		run "cd #{current_release} && cake schema create atlas < #{shared_path}/config/schema_create_prompt.txt"
+	end
+	
+	desc "Update database schema update tables"
+	task :migrate_database_update, roles => [:web] do
+		run "cd #{current_release} && yes y | cake schema update atlas"
+	end  
+	
+	task :finalize_deploy, :roles => [:web] do
+		run "chmod 755 -R #{release_path}"
+		run "mv #{release_path}/webroot/index.staging.php #{release_path}/webroot/index.php"
+		run "ln -s #{shared_path}/system #{release_path}/webroot/"
+		run "mv #{release_path}/config/atlas.default.php #{release_path}/config/atlas.php"
+		run "mv #{release_path}/config/core.default.php #{release_path}/config/core.php"
+	end	
 
-desc "Update database schema update tables"
-task :migrate_database_update, roles => [:web] do
-	run "cd #{current_release} && yes y | cake schema update atlas"
-end
 
 after "deploy:update_code", :finalize_deploy
 
