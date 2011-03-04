3445645b84fa2ab44a6c0f0d5d6dcc5df036539c
Brandon Cordell <brandonc@ctsfla.com>
2011-03-04 10:29:09 -0500

    Added validation to rfp model

--- models/rfp.php
+++ models/rfp.php
@@ -2,5 +2,38 @@
 class Rfp extends AppModel {
 	var $name = 'Rfp';
 	var $displayField = 'title';
+	
+	var $validate = array(
+		'title' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must include a title'
+			)
+		),
+		'byline' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must include a byline'
+			)		
+		),
+		'description' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must include a description'
+			)		
+		),
+		'deadline' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must include a deadline'
+			)		
+		),
+		'expires' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must include a expiration'
+			)		
+		),
+	);
 }
 ?>
