afa783d89dd90114c60ce9b243022c07f7c8119f
Brandon Cordell <brandonc@ctsfla.com>
2011-03-07 15:31:21 -0500

    removed methods we dont use

--- controllers/helpful_articles_controller.php
+++ controllers/helpful_articles_controller.php
@@ -8,48 +8,6 @@ class HelpfulArticlesController extends AppController {
 		$this->set('helpfulArticles', $this->paginate());
 	}
 
-	function add() {
-		if (!empty($this->data)) {
-			$this->HelpfulArticle->create();
-			if ($this->HelpfulArticle->save($this->data)) {
-				$this->Session->setFlash(__('The helpful article has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The helpful article could not be saved. Please, try again.', true), 'flash_failure');
-			}
-		}
-	}
-
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid helpful article', true), 'flash_failure');
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->HelpfulArticle->save($this->data)) {
-				$this->Session->setFlash(__('The helpful article has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The helpful article could not be saved. Please, try again.', true), 'flash_failure');
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->HelpfulArticle->read(null, $id);
-		}
-	}
-
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for helpful article', true), 'flash_failure');
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->HelpfulArticle->delete($id)) {
-			$this->Session->setFlash(__('Helpful article deleted', true), 'flash_success');
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Helpful article was not deleted', true), 'flash_failure');
-		$this->redirect(array('action' => 'index'));
-	}
 	function admin_index() {
 		$this->HelpfulArticle->recursive = 0;
 		$this->set('helpfulArticles', $this->paginate());
