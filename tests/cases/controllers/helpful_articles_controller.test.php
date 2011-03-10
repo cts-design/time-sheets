8e22857937f2844d0c8ea5aa10beff51f1daf631
Brandon Cordell <brandonc@ctsfla.com>
2011-03-10 10:39:21 -0500

    finished helpful articles controller test
    re #32

--- tests/cases/controllers/helpful_articles_controller.test.php
+++ tests/cases/controllers/helpful_articles_controller.test.php
@@ -27,33 +27,56 @@ class HelpfulArticlesControllerTestCase extends AtlasTestCase {
 
 	}
 
-	function testAdd() {
+	function testAdminEditWithValidData() {
+        $this->HelpfulArticles->data = array(
+            'HelpfulArticle' => array(
+                'title' => 'Valid Title'
+            )
+        );
 
+        $this->HelpfulArticles->admin_edit(1);
+        $this->assertEqual($this->HelpfulArticles->redirectUrl, array('action' => 'index'));
+
+		$this->assertFlashMessage($this->HelpfulArticles, 'The helpful article has been saved', 'flash_success');
 	}
 
-	function testEdit() {
+    function testAdminEditWithInvalidData() {
+        $this->HelpfulArticles->data = array(
+            'HelpfulArticle' => array(
+                'title' => ''
+            )
+        );
 
+        $this->HelpfulArticles->admin_edit(1);
+		$this->assertFlashMessage($this->HelpfulArticles, 'The helpful article could not be saved. Please, try again.', 'flash_failure');
+    }
+
+    function testAdminEditInvalidRecord() {
+        $this->HelpfulArticles->admin_edit();
+        $this->assertEqual($this->HelpfulArticles->redirectUrl, array('action' => 'index'));
+		
+		$this->assertFlashMessage($this->HelpfulArticles, 'Invalid helpful article', 'flash_failure');
+    }
+
+	function testAdminDeleteValidRecord() {
+            $this->HelpfulArticles->admin_delete(1);
+            $this->assertEqual($this->HelpfulArticles->redirectUrl, array('action' => 'index'));
+            $this->assertFalse($this->HelpfulArticles->HelpfulArticle->read(null, 1));
 	}
 
-	function testDelete() {
+    function testAdminDeleteInvalidRecord() {
+        $this->HelpfulArticles->admin_delete(100);
+        $this->assertEqual($this->HelpfulArticles->redirectUrl, array('action' => 'index'));
 
-	}
+		$this->assertFlashMessage($this->HelpfulArticles, 'Helpful article was not deleted', 'flash_failure');
+    }
 
-	function testAdminIndex() {
-
-	}
-
-	function testAdminAdd() {
-
-	}
-
-	function testAdminEdit() {
-
-	}
-
-	function testAdminDelete() {
-
-	}
+    function testAdminDeleteWithNoSpecifiedRecord() {
+        $this->HelpfulArticles->admin_delete();
+		
+		$this->assertFlashMessage($this->HelpfulArticles, 'Helpful article was not deleted', 'flash_failure');
+        $this->assertEqual($this->HelpfulArticles->redirectUrl, array('action' => 'index'));
+   }
 
 }
 ?>
