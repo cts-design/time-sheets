72db7df32e6e461ecf99c02edb96d919c2e1cfe4
Brandon Cordell <brandonc@ctsfla.com>
2011-03-10 10:00:07 -0500

    updated the helpful articles validation and wrote the test for the model
    re #32

--- models/helpful_article.php
+++ models/helpful_article.php
@@ -2,5 +2,38 @@
 class HelpfulArticle extends AppModel {
 	var $name = 'HelpfulArticle';
 	var $displayField = 'title';
+	
+	var $validate = array(
+		'title' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a title'
+			)
+		),
+		'reporter' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide the articles reporter'
+			)
+		),
+		'summary' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a summary'
+			)
+		),
+		'link' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a link to the article'
+			)
+		),
+		'posted_date' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide the date the article was posted'
+			)
+		),
+	);
 }
 ?>
