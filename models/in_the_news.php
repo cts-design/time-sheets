4205528052049523edd345f89c927768d9edf71f
Brandon Cordell <brandonc@ctsfla.com>
2011-03-10 10:43:58 -0500

    added validation to the inthenews model, and completed the model test cases
    re #31

--- models/in_the_news.php
+++ models/in_the_news.php
@@ -2,5 +2,38 @@
 class InTheNews extends AppModel {
 	var $name = 'InTheNews';
 	var $displayField = 'title';
+	
+	var $validate = array(
+		'title' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a title'
+			)
+		),
+		'reporter' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide the articles reporter'
+			)
+		),
+		'summary' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a summary'
+			)
+		),
+		'link' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide a link to the article'
+			)
+		),
+		'posted_date' => array(
+			'notEmpty' => array(
+				'rule' => 'notEmpty',
+				'message' => 'You must provide the date the article was posted'
+			)
+		),
+	);
 }
 ?>
