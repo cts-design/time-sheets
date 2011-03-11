ad6e9b5149f4523781199b3028d2620f081bb00d
Brandon Cordell <brandonc@ctsfla.com>
2011-03-11 11:13:43 -0500

    created a surveyBuilder js file
    re #30

---
+++ webroot/js/surveys/survey_builder.js
@@ -0,0 +1,10 @@
+/**
+ * @author Brandon Cordell
+ * @copyright Complete Technology Solutions 2011
+ * @link http://ctsfla.com
+ */
+
+SurveyBuilder = function() {};
+
+SurveyBuilder.prototype = {
+}
