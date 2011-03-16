4da0f819f75ba46904810eab3a19b606bdb8bc66
Daniel Nolan <daniel@ctsfla.com>
2011-03-16 14:43:45 -0400

    wrote tests for FiledDocumentModel
    time: 1h42m
    
    re #41

--- tests/cases/models/filed_document.test.php
+++ tests/cases/models/filed_document.test.php
@@ -3,7 +3,36 @@
 App::import('Model', 'FiledDocument');
 
 class FiledDocumentTestCase extends CakeTestCase {
-	var $fixtures = array('app.filed_document', 'app.user', 'app.role', 'app.location', 'app.ftp_document_scanner', 'app.kiosk', 'app.kiosk_button', 'app.master_kiosk_button', 'app.self_sign_log', 'app.self_sign_log_archive', 'app.queued_document', 'app.document_queue_category', 'app.user_transaction');
+		var $fixtures = array(
+	    'app.aco',
+	    'app.aro',
+	    'app.aros_aco',
+	    'chairman_report',
+	    'deleted_document',
+	    'document_filing_category',
+	    'document_queue_category',
+	    'document_transaction',
+	    'featured_employer',
+	    'filed_document',
+	    'ftp_document_scanner',
+	    'hot_job',
+	    'kiosk',
+	    'kiosk_button',
+	    'location',
+	    'master_kiosk_button',
+	    'module_access_control',
+	    'navigation',
+	    'page',
+	    'press_release',
+	    'queued_document',
+	    'rfp',
+	    'role',
+	    'self_scan_category',
+	    'self_sign_log',
+	    'self_sign_log_archive',
+	    'user',
+	    'user_transaction'
+	);
 
 	function startTest() {
 		$this->FiledDocument =& ClassRegistry::init('FiledDocument');
@@ -13,6 +42,41 @@ class FiledDocumentTestCase extends CakeTestCase {
 		unset($this->FiledDocument);
 		ClassRegistry::flush();
 	}
-
+	
+	function testValidation() {
+		$this->FiledDocument->create();		
+		$invalidData = array(
+			'FiledDocument' => array(
+				'id' => '',
+				'filename' => '',
+				'admin_id' => '',
+				'user_id' => 12,
+				'filed_location_id' => '',
+				'cat_1' => 1,
+				'cat_2' => 2,
+				'cat_3'	=> 3,
+				'entry_method' => 'Scan',
+				'last_activity_admin' => 2
+			)
+		);			
+		$this->FiledDocument->save($invalidData);
+		$invalidFields = $this->FiledDocument->invalidFields();
+		$this->assertEqual($invalidFields['filename'], 'Filename is required');
+		$this->assertEqual($invalidFields['admin_id'], 'Admin Id required');
+		$this->assertEqual($invalidFields['filed_location_id'], 'Filed location is required');
+		$this->assertEqual($invalidFields['id'], 'Id is required');
+	}
+	
+	function testBeforeDelete() {
+		$data = array(
+			'FiledDocument' => array(
+				'last_activity_admin_id' => 1,
+				'reason' => 'Duplicate Scan',
+				'deleted_location_id' => 1
+			)
+		);
+		$this->FiledDocument->set($data);
+		$this->assertTrue($this->FiledDocument->delete(1));	
+	}
 }
 ?>
