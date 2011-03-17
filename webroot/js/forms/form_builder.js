ac8fc9f94c897d5a8b21a00e695f63b9a8942b1b
Brandon Cordell <brandonc@ctsfla.com>
2011-03-17 11:03:09 -0400

    added form_builder.js
    re #42

---
+++ webroot/js/forms/form_builder.js
@@ -0,0 +1,11 @@
+/**
+ * @author Brandon Cordell
+ * @copyright Complete Technology Solutions 2011
+ * @link http://ctsfla.com
+ * @package ATLAS V3
+ */
+ 
+FormBuilder = function() {};
+
+FormBuilder.prototype = {
+}
