c635ec018a190e20dbb244244c07df6298063cca
Brandon Cordell <brandonc@ctsfla.com>
2011-03-23 15:45:06 -0400

    added employers surveys table to the schema file
    re #30

--- config/schema/atlas.php
+++ config/schema/atlas.php
@@ -1,5 +1,5 @@
 <?php 
-/* atlas schema generated on: 2011-03-21 16:23:25 : 1300724605*/
+/* atlas schema generated on: 2011-03-23 15:44:26 : 1300909466*/
 class atlasSchema extends CakeSchema {
 	var $name = 'atlas';
 
@@ -119,6 +119,14 @@ class atlasSchema extends CakeSchema {
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+	var $employers_surveys = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'answers' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
 	);
 	var $event_categories = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
