0e2acc68c693b2a36e071410369bb073fbdf9b9e
Brandon Cordell <brandonc@ctsfla.com>
2011-03-23 15:21:43 -0400

    completed the add and read methods
    re #30
    Time: 1h30m

--- controllers/career_seekers_surveys_controller.php
+++ controllers/career_seekers_surveys_controller.php
@@ -4,98 +4,44 @@ class CareerSeekersSurveysController extends AppController {
 	var $name = 'CareerSeekersSurveys';
 
 	function index() {
-		$this->CareerSeekersSurvey->recursive = 0;
-		$this->set('careerSeekersSurveys', $this->paginate());
+
 	}
 
 	function add() {
 		if (!empty($this->data)) {
+			$data = json_encode($this->data['CareerSeekersSurvey']);
+			$this->data['CareerSeekersSurvey']['answers'] = $data;
 			$this->CareerSeekersSurvey->create();
 			if ($this->CareerSeekersSurvey->save($this->data)) {
-				$this->Session->setFlash(__('The career seekers survey has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
+				$this->Session->setFlash(__('The career seekers survey has been submitted', true), 'flash_success');
+				$this->redirect(array('action' => 'success'));
 			} else {
-				$this->Session->setFlash(__('The career seekers survey could not be saved. Please, try again.', true), 'flash_failure');
+				$this->Session->setFlash(__('The career seekers survey could not be submitted. Please, try again.', true), 'flash_failure');
 			}
 		}
 	}
-
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid career seekers survey', true), 'flash_failure');
-			$this->redirect(array('action' => 'index'));
+	
+	function success() {}
+	
+	function admin_read() {
+		$surveys = $this->CareerSeekersSurvey->find('all');
+		$i = 0;
+		foreach ($surveys as $key => $value) {
+			$value['CareerSeekersSurvey']['answers'] = json_decode($value['CareerSeekersSurvey']['answers'], true);
+			$data['surveys'][] = $value['CareerSeekersSurvey'];
+			debug($value['CareerSeekersSurvey']['answers']);
+			$i++;
 		}
-		if (!empty($this->data)) {
-			if ($this->CareerSeekersSurvey->save($this->data)) {
-				$this->Session->setFlash(__('The career seekers survey has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The career seekers survey could not be saved. Please, try again.', true), 'flash_failure');
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->CareerSeekersSurvey->read(null, $id);
-		}
+		
+		debug($data);
+		
+		$this->set('data', $data);
+		return $this->render(null, null, '/elements/ajaxreturn');
 	}
-
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for career seekers survey', true), 'flash_failure');
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->CareerSeekersSurvey->delete($id)) {
-			$this->Session->setFlash(__('Career seekers survey deleted', true), 'flash_success');
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Career seekers survey was not deleted', true), 'flash_failure');
-		$this->redirect(array('action' => 'index'));
-	}
+	
 	function admin_index() {
 		$this->CareerSeekersSurvey->recursive = 0;
 		$this->set('careerSeekersSurveys', $this->paginate());
-	}
-
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->CareerSeekersSurvey->create();
-			if ($this->CareerSeekersSurvey->save($this->data)) {
-				$this->Session->setFlash(__('The career seekers survey has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The career seekers survey could not be saved. Please, try again.', true), 'flash_failure');
-			}
-		}
-	}
-
-	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid career seekers survey', true), 'flash_failure');
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->CareerSeekersSurvey->save($this->data)) {
-				$this->Session->setFlash(__('The career seekers survey has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The career seekers survey could not be saved. Please, try again.', true), 'flash_failure');
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->CareerSeekersSurvey->read(null, $id);
-		}
-	}
-
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for career seekers survey', true), 'flash_failure');
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->CareerSeekersSurvey->delete($id)) {
-			$this->Session->setFlash(__('Career seekers survey deleted', true), 'flash_success');
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Career seekers survey was not deleted', true), 'flash_failure');
-		$this->redirect(array('action' => 'index'));
 	}
 }
 ?>
