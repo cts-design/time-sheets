6f7d9d0e7a281bd1a9a9e41f0a306ac70105111b
Daniel Nolan <daniel@ctsfla.com>
2011-03-24 16:27:48 -0400

    added logic to the index function to check for a program id re# 75
    time: 0h10m

--- controllers/program_controllers/programs_controller.php
+++ controllers/program_controllers/programs_controller.php
@@ -1,9 +1,12 @@
 <?php
 
 class ProgramsController extends AppController {
-	
-	
-	function index() {
+		
+	function index($id = null) {
+		if(!$id) {
+			$this->Session->setFlash(__('Invalid Program Id', true), 'flash_failure');
+			$this->redirect('/');
+		}
 		$program = $this->Program->findById('1');
 		$this->set(compact('program'));
 	}
