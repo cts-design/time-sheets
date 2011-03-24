ab5fe5b540073a053aa167fe70d75ae323287661
Daniel Nolan <daniel@ctsfla.com>
2011-03-24 15:24:26 -0400

    added logic to used passed in program id instead of hardcoded values I was using for testing
    re #76

--- controllers/program_controllers/program_registrations_controller.php
+++ controllers/program_controllers/program_registrations_controller.php
@@ -5,25 +5,34 @@ class ProgramRegistrationsController extends AppController {
 	function beforeFilter() {
 		parent::beforeFilter();
 		$this->ProgramRegistration->Program->ProgramField->recursive = -1;
-		$query = $this->ProgramRegistration->Program->ProgramField->findAllByProgramId(1);
-		$fields = Set::classicExtract($query, '{n}.ProgramField');
-		foreach($fields as $k => $v) {
-			if(!empty($v['validation'])) 
-				$validate[$v['name']] = json_decode($v['validation'], true); 
+		if(!empty($this->params['pass'][0])) {
+			$query = $this->ProgramRegistration->Program->ProgramField->findAllByProgramId($this->params['pass'][0]);			
+			$fields = Set::classicExtract($query, '{n}.ProgramField');
+			foreach($fields as $k => $v) {
+				if(!empty($v['validation'])) 
+					$validate[$v['name']] = json_decode($v['validation'], true); 
+			}
+			$this->ProgramRegistration->modifyValidate($validate);
 		}
-		$this->ProgramRegistration->modifyValidate($validate);
 	}	
 	
-	function index() {
+	function index($id = null) {
+		if(!$id) {
+			$this->Session->setFlash(__('Invalid Program Id', true), 'flash_failure');
+			$this->redirect($this->referer());
+		} 
 		if(!empty($this->data)) {
-			if($this->ProgramRegistration->save($this->data)) {
+			$this->ProgramRegistration->create();
+			$data['ProgramRegistration']['answers'] = json_encode($this->data['ProgramRegistration']);
+			$data['ProgramRegistration']['program_id'] = $id;
+			if($this->ProgramRegistration->save($data)) {
 				$this->Session->setFlash(__('Saved', true), 'flash_success');
 			}
 			else {
 				$this->Session->setFlash(__('Unable to save', true), 'flash_failure');
 			}			
 		}
-		$program = $this->ProgramRegistration->Program->findById('1');
+		$program = $this->ProgramRegistration->Program->findById($id);
 		$title_for_layout = $program['Program']['name'] . ' Registration Form' ;
 		$this->set(compact('program', 'title_for_layout'));	
 	}
