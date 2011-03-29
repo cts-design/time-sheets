37bc1ac28a9d279c1595ebc3e258b353986ece61
Daniel Nolan <daniel@ctsfla.com>
2011-03-29 13:56:44 -0400

    added test data to fixture
    re #76

--- tests/fixtures/program_registration_fixture.php
+++ tests/fixtures/program_registration_fixture.php
@@ -1,9 +1,22 @@
 <?php
-/* ProgramRegistration Fixture generated on: 2011-03-28 21:51:46 : 1301349106 */
+/* ProgramRegistration Fixture generated on: 2011-03-29 17:26:26 : 1301419586 */
 class ProgramRegistrationFixture extends CakeTestFixture {
 	var $name = 'ProgramRegistration';
-	var $import = array('model' => 'ProgramRegistration', 'records' => true);
 
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'program_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
+		'answers' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'prog_id' => array('column' => 'program_id', 'unique' => 0)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
 
+	var $records = array(
+		array(
+			'id' => 1,
+			'program_id' => 1,
+			'answers' => '{\"preferred_program_schedule\":\"Winter\\/Spring only\",\"preferred_program_setting\":\"Public school\"}'
+		),
+	);
 }
 ?>
