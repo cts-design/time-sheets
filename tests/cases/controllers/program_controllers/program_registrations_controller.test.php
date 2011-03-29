03abdf66d93a5033530f2bb6e3216d0ffb6ec61a
Daniel Nolan <daniel@ctsfla.com>
2011-03-29 13:54:51 -0400

    added tests for index and index with no id
    re #76
    
    time: 0h45m

--- tests/cases/controllers/program_controllers/program_registrations_controller.test.php
+++ tests/cases/controllers/program_controllers/program_registrations_controller.test.php
@@ -11,13 +11,41 @@ class TestProgramRegistrationsController extends ProgramRegistrationsController 
 }
 
 class ProgramRegistrationsControllerTestCase extends CakeTestCase {
-	var $fixtures = array('navigation', 'program_registration');
+	var $fixtures = array(
+            'app.aco',
+            'app.aro',
+            'app.aro_aco',
+            'app.chairman_report',
+            'app.deleted_document',
+            'app.document_filing_category',
+            'app.document_queue_category',
+            'app.document_transaction',
+            'app.filed_document',
+            'app.ftp_document_scanner',
+            'app.kiosk',
+            'app.kiosk_button',
+            'app.location',
+            'app.master_kiosk_button',
+            'app.module_access_control',
+            'app.navigation',
+            'app.page',
+            'app.press_release',
+            'app.program',
+            'app.program_registration',
+            'app.program_field',
+            'app.queued_document',
+            'app.role',
+            'app.self_scan_category',
+            'app.self_sign_log',
+            'app.self_sign_log_archive',
+            'app.user',
+            'app.user_transaction'
+        );
+
 
 	function startTest() {
 		$this->ProgramRegistrations =& new TestProgramRegistrationsController();
 		$this->ProgramRegistrations->constructClasses();
-		debug($this->ProgramRegistrations);	
-
 	}
 
 	function endTest() {
@@ -26,7 +54,54 @@ class ProgramRegistrationsControllerTestCase extends CakeTestCase {
 	}
 	
 	function testIndex() {
-			
+		$this->ProgramRegistrations->Session->write('Auth.User', array(
+	        'id' => 9,
+	        'role_id' => 1,
+	        'username' => 'smith'
+	    ));
+		
+		$expectedResult = array(
+			array(
+				'id' => 1,
+				'program_id' => 1,
+				'label' => 'Preferred program schedule',
+				'type' => 'select',
+				'name' => 'preferred_program_schedule',
+				'attributes' => '{\"class\":\"testClass\"}',
+				'options' => '{\"\" : \"Select\", \"Winter\\/Spring only\":\"Winter\\/Spring only (540 Hours)\",\"School Year\":\"School Year (540 Hours)\", \"Summer Program\":\"Summer Program (300 Hours)\", \"Fall\\/Winter only\":\"Fall\\/Winter only (540 Hours)\"}',
+				'validation' => '{\"rule\":\"notEmpty\"}',
+				'created' => '2011-03-23 16:42:20',
+				'modified' => '2011-03-24 16:42:24'
+			),
+			array(
+				'id' => 2,
+				'program_id' => 1,
+				'label' => 'Preferred program setting',
+				'type' => 'select',
+				'name' => 'preferred_program_setting',
+				'attributes' => NULL,
+				'options' => '{\"\":\"Select\", \"Private provider\":\"Private provider (child care, private school, faith-based)\", \"Public school\":\"Public school\"}',
+				'validation' => '{\"rule\":\"notEmpty\"}',
+				'created' => '2011-03-24 15:01:17',
+				'modified' => '2011-03-24 15:01:22'
+			),
+		);
+		
+		$result = $this->testAction('/program_registrations/index/1', array('return' => 'vars'));
+		$result = Set::extract('ProgramField', $result['program']);
+		$this->assertEqual($result, $expectedResult);
+	}
+	
+	function testIndexNoId() {
+		$this->ProgramRegistrations->Session->write('Auth.User', array(
+	        'id' => 9,
+	        'role_id' => 1,
+	        'username' => 'smith'
+	    ));
+	    $this->ProgramRegistrations->params = Router::parse('/program_registrations/index');
+	    $this->ProgramRegistrations->Component->startup($this->ProgramRegistrations);
+		$this->ProgramRegistrations->index();	
+		$this->assertEqual($this->ProgramRegistrations->Session->read('Message.flash.element'), 'flash_failure');		
 	}
 
 }
