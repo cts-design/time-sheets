8e337067de5954d6be1e0bd8ea5df3877cdb4262
Daniel Nolan <daniel@ctsfla.com>
2011-03-31 17:05:46 -0400

    removed unused action

--- controllers/program_controllers/program_responses_controller.php
+++ controllers/program_controllers/program_responses_controller.php
@@ -24,18 +24,46 @@ class ProgramResponsesController extends AppController {
 			$this->redirect($this->referer());
 		} 
 		if(!empty($this->data)) {
-			$this->ProgramResponse->create();
+			$response = $this->ProgramResponse->findByUserId($this->Auth->user('id'));
+			$this->data['ProgramResponse']['id'] = $response['ProgramResponse']['id'];
 			$this->data['ProgramResponse']['answers'] = json_encode($this->data['ProgramResponse']);
 			$this->data['ProgramResponse']['program_id'] = $id;
 			if($this->ProgramResponse->save($this->data)) {
 				$this->Session->setFlash(__('Saved', true), 'flash_success');
+				$program = $this->ProgramResponse->Program->findById($id);
+				if(strpos($program['Program']['type'], 'docs', 0)) {
+					$this->redirect(array('action' => 'required_docs', $id));
+				}
+				else{
+					$this->redirect(array('action' => 'submission_received'));
+				}
 			}
 			else {
 				$this->Session->setFlash(__('Unable to save', true), 'flash_failure');
 			}			
 		}
 		$program = $this->ProgramResponse->Program->findById($id);
+		$instructions = $program['Program']['form_instructions'];
 		$title_for_layout = $program['Program']['name'] . ' Registration Form' ;
-		$this->set(compact('program', 'title_for_layout'));	
+		$this->set(compact('program', 'title_for_layout', 'instructions'));	
+	}
+		
+	function required_docs($id = null) {
+		if(!$id){
+			$this->Session->setFlash(__('Invalid Program Id', true), 'flash_failure');
+			$this->redirect($this->referer());
+		}
+		$program = $this->ProgramResponse->Program->findById($id);
+		$data['instructions'] = $program['Program']['doc_instructions'];
+		$data['title_for_layout'] = 'Required Documentation';
+		$this->set($data);
+	}
+	
+	function submission_received() {
+		
+	}
+	
+	function document_uploaded() {
+		
 	}
 }
