c83e378f56c6f6fc1f59ef27fef7e4cb316da5c9
Daniel Nolan <daniel@ctsfla.com>
2011-04-04 10:37:53 -0400

    added controller and model for ProgramEmails
    re #75
    time: 0h5m

---
+++ tests/fixtures/program_email_fixture.php
@@ -0,0 +1,32 @@
+<?php
+/* ProgramEmail Fixture generated on: 2011-04-04 14:35:21 : 1301927721 */
+class ProgramEmailFixture extends CakeTestFixture {
+	var $name = 'ProgramEmail';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'program_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
+		'subject' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'body' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 25, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'program_id' => array('column' => 'program_id', 'unique' => 0)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'program_id' => 1,
+			'subject' => 'Lorem ipsum dolor sit amet',
+			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'type' => 'Lorem ipsum dolor sit a',
+			'name' => 'Lorem ipsum dolor sit amet',
+			'created' => '2011-04-04 14:35:21',
+			'modified' => '2011-04-04 14:35:21'
+		),
+	);
+}
+?>
