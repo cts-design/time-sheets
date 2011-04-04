15243853e8653c51b0abe9676a9d20ba40ff19b9
Brandon Cordell <brandonc@ctsfla.com>
2011-04-04 10:23:31 -0400

    updated event and event_category fixtures to match current db records

--- tests/fixtures/event_category_fixture.php
+++ tests/fixtures/event_category_fixture.php
@@ -1,5 +1,5 @@
 <?php
-/* EventCategory Fixture generated on: 2011-02-23 19:40:20 : 1298490020 */
+/* EventCategory Fixture generated on: 2011-04-01 15:56:04 : 1301687764 */
 class EventCategoryFixture extends CakeTestFixture {
 	var $name = 'EventCategory';
 
@@ -15,9 +15,33 @@ class EventCategoryFixture extends CakeTestFixture {
 	var $records = array(
 		array(
 			'id' => 1,
-			'name' => 'Lorem ipsum dolor sit amet',
-			'created' => '2011-02-23 19:40:20',
-			'modified' => '2011-02-23 19:40:20'
+			'name' => 'Board Meetings',
+			'created' => '2011-02-23 19:58:30',
+			'modified' => '2011-02-23 19:58:30'
+		),
+		array(
+			'id' => 2,
+			'name' => 'Business Seminars',
+			'created' => '2011-02-23 20:04:22',
+			'modified' => '2011-02-23 20:04:22'
+		),
+		array(
+			'id' => 3,
+			'name' => 'Job Fairs',
+			'created' => '2011-02-23 20:05:28',
+			'modified' => '2011-02-23 20:05:28'
+		),
+		array(
+			'id' => 4,
+			'name' => 'Networking Events',
+			'created' => '2011-02-23 20:05:51',
+			'modified' => '2011-02-23 20:06:41'
+		),
+		array(
+			'id' => 6,
+			'name' => 'Workshops',
+			'created' => '2011-02-23 20:10:24',
+			'modified' => '2011-02-23 20:10:24'
 		),
 	);
 }
