b24b89efb4d3a262368e0ade014215153f58ab22
Brandon Cordell <brandonc@ctsfla.com>
2011-04-06 16:57:53 -0400

    Got the form_builder specifying the proper attributes for each field. Changed the buttons around, and added more field types. Added a right click context menu to the formPanel.
    Time: 5h46m

--- webroot/js/program_fields/form_builder.js
+++ webroot/js/program_fields/form_builder.js
@@ -20,13 +20,39 @@ FormBuilder.prototype = {
 		
 		this.initDataStores();
 		this.initPanels();
+		
+		// init menu
+		this.contextMenu = new Ext.menu.Menu({
+			items: [{
+				text: 'Edit'
+			},{
+				text: 'Another'
+			}]
+		});
 	},
 	initDataStores: function() {
-		
+		this.tempOptionsStore = new Ext.data.ArrayStore({
+			idIndex: 0,
+			storeId: 'FBOptionsStore',
+			fields: [
+				'options'
+			]
+		});
 	},
 	initPanels: function() {		
 		var tb = new Ext.Toolbar({
 			items: [{
+				id: 'FBSaveBtn',
+				text: 'Save Form',
+				icon: '/img/icons/save.png',
+				tooltip: 'Save the form',
+				scope: this,
+				handler: function() {
+					this.saveForm();
+				}				
+			},
+			'-',
+			{
 				id: 'FBResetBtn',
 				text: 'Reset Form',
 				icon: '/img/icons/reset.png',
@@ -42,22 +68,107 @@ FormBuilder.prototype = {
 			xtype: 'button',
 			text: 'Textfield',
 			scope: this,
+			icon: '/img/icons/input_text.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
 			handler: function() {
 				this.addField('textfield');
 			}
 		},{
 			xtype: 'button',
+			text: 'Textarea',
+			scope: this,
+			icon: '/img/icons/input_textarea.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			scale: 'medium',
+			handler: function() {
+				this.addField('textarea');
+			}
+		},{
+			xtype: 'button',
 			text: 'Dropdown',
 			scope: this,
+			icon: '/img/icons/input_select.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
 			handler: function() {
 				this.addField('combo');
+			}
+		},{
+			xtype: 'button',
+			text: 'Radio Group',
+			scope: this,
+			icon: '/img/icons/input_radio.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			scale: 'medium',
+			handler: function() {
+				this.addField('textarea');
+			}
+		},{
+			xtype: 'button',
+			text: 'Checkbox Group',
+			scope: this,
+			icon: '/img/icons/input_checkbox.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			scale: 'medium',
+			handler: function() {
+				this.addField('textarea');
+			}
+		},{
+			xtype: 'button',
+			text: 'Submit Button',
+			scope: this,
+			icon: '/img/icons/input_submit.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			scale: 'medium',
+			handler: function() {
+				this.addField('textarea');
 			}
 		}];
 		
 		var quickButtons = [{
 			xtype: 'button',
-			text: 'Datefield',
+			text: 'Full Name',
 			scope: this,
+			icon: '/img/icons/input_fullname.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			handler: function() {
+				this.addField('textfield');
+			}
+		},{
+			xtype: 'button',
+			text: 'Email',
+			scope: this,
+			icon: '/img/icons/input_email.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			scale: 'medium',
+			handler: function() {
+				this.addField('textarea');
+			}
+		},{
+			xtype: 'button',
+			text: 'Address',
+			scope: this,
+			icon: '/img/icons/input_address.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			handler: function() {
+				this.addField('combo');
+			}
+		},{
+			xtype: 'button',
+			text: 'Datepicker',
+			scope: this,
+			icon: '/img/icons/input_datepicker.png',
+			cls: 'accordionbtn',
+			overCls: 'x-btn-hover',
+			scale: 'medium',
 			handler: function() {
 				this.addField('datefield');
 			}
@@ -68,7 +179,7 @@ FormBuilder.prototype = {
 			tbar: tb,
 			applyTo: 'form-builder',
 			title: 'Form Builder',
-			height: 400,
+			height: 500,
 			layout: 'border',
 			items: [{
 				id: 'FBTools',
@@ -77,9 +188,9 @@ FormBuilder.prototype = {
 				items: [{
 					layout: 'accordion',
 					border: false,
-					height: 150,
+					height: 250,
 					defaults: {
-						bodyStyle: 'padding: 8px'
+						bodyStyle: 'padding:8px'
 					},
 					items: [{
 						title: 'Form Tools',
@@ -100,7 +211,9 @@ FormBuilder.prototype = {
 				   	listeners: {
 				   		afteredit: {
 				   			fn: function(e) {
+				   				var store = this.tempOptionsStore;
 				   				console.log(e);
+				   				
 				   				switch (e.record.id) {
 				   					case 'Label':
 				   						this.selectedElement.label.dom.textContent = e.value + ":";
@@ -108,6 +221,32 @@ FormBuilder.prototype = {
 				   					case 'Default Value':
 				   						this.selectedElement.el.dom.defaultValue = e.value;
 				   						break;
+				   					case 'Options':
+				   						var records = [],
+				   						values = e.value.split(',');
+				   						store.removeAll();
+				   						
+				   						Ext.each(values, function(item, index) {
+				   							store.insert(index, new store.recordType({options: item.trim()}));
+				   						}, this);
+										break;
+				   				}
+				   			},
+				   			scope: this
+				   		},
+				   		validateedit: {
+				   			fn: function(e) {
+				   				if (e.record.id === 'Selected') {
+									var options = this.selectedElement.attributes['Options'].split(',');
+									
+									Ext.each(options, function(item, index) {
+										options[index] = item.trim();
+									}, this);
+									
+									if (options.indexOf(e.value) === -1) {
+										Ext.Msg.alert('Invalid Value For Selected', 'The selected value must be in your options');
+										return false;
+									}
 				   				}
 				   			},
 				   			scope: this
@@ -137,7 +276,12 @@ FormBuilder.prototype = {
 				}, this);
 				console.log(formElement);
 			}
-		}, this)
+		}, this);
+		
+		this.formPanel.el.on('contextmenu', function(e) {
+			e.preventDefault();
+			this.contextMenu.showAt(e.getXY());
+		}, this);
 	},
 	addField: function(fieldType) {
 		var config = {
@@ -147,41 +291,42 @@ FormBuilder.prototype = {
 		
 		var attributes = {
 			'Label': 'Field Label',
-			'Default Value': '',
 			'Class': ''			
 		};
 		
 		switch (fieldType) {
 			case 'textfield':
+				attributes['Default Value'] = '';
 				attributes['Max Length'] = '';
-				attributes['Min Length'] = '';
-				attributes['Class'] = 'text';
+				attributes['Class'] = '';
+				break;
+				
+			case 'textarea':
+				attributes['Cols'] = '';
+				attributes['Rows'] = '';
+				
+				config.height = 75;
+				config.width = 250;
 				break;
 				
 			case 'datefield':
 				attributes['Date Format'] = ['m/d/y','d/m/y', 'y/m/d'];
 				attributes['Min Year'] = '';
 				attributes['Max Year'] = '';
-				attributes['Class'] = 'date';
+				attributes['Class'] = '';
 				break;
 				
 			case 'combo':
+				attributes['Selected'] = '';
+			   	attributes['Options'] = '';
+				
 				config.typeAhead = false;
 				config.triggerAction = 'all';
 				config.mode = 'local';
-				config.store = new Ext.data.ArrayStore({
-			        id: 0,
-			        fields: [
-						'options'
-			        ],
-			        data: [['item1'], ['item2']]
-			   	});
-			   	config.valueField = 'myId';
-			   	config.displayField = 'displayText';
-			   	
-			   	attributes['Selected'] = '';
-			   	attributes['Options'] = config.store.data.join(',');
-				break;
+				config.store = this.tempOptionsStore;
+			   	config.valueField = 'options';
+			   	config.displayField = 'options';
+			   	break;
 		}
 		
 		var newField = this.formPanel.add(config);
@@ -203,6 +348,11 @@ FormBuilder.prototype = {
 				}
 			}
 		});
+	},
+	saveForm: function() {
+		Ext.each(this.formPanel.items.items, function(item, index, allItems) {
+			console.log(item.attributes);
+		}, this)
 	}
 }
 
