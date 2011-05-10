cdd3b2198b2172f3e61406fa2adf9be441cd5638
Daniel Nolan <daniel@ctsfla.com>
2011-05-10 15:08:10 -0400

    baked ProgramInstruction model and test files
    re #75

---
+++ tests/fixtures/program_instruction_fixture.php
@@ -0,0 +1,28 @@
+<?php
+/* ProgramInstruction Fixture generated on: 2011-05-10 13:53:30 : 1305050010 */
+class ProgramInstructionFixture extends CakeTestFixture {
+	var $name = 'ProgramInstruction';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'program_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
+		'text' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'program_id' => array('column' => 'program_id', 'unique' => 0)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'program_id' => 1,
+			'text' => 1,
+			'type' => 'Lorem ipsum dolor sit amet',
+			'created' => '2011-05-10 13:53:30',
+			'modified' => '2011-05-10 13:53:30'
+		),
+	);
+}
+?>
