9dd4c11459e41ce0bff9710744fac041322d1c98
Daniel Nolan <daniel@ctsfla.com>
2011-05-26 16:23:40 -0400

    updating edit link urls

--- webroot/js/programs/admin_instructions_index.js
+++ webroot/js/programs/admin_instructions_index.js
@@ -5,23 +5,23 @@ Ext.onReady(function(){
 	var instructions = [
 		[
 			'Program Main Instructions', 
-			'<a href="/admin/programs/edit_instructions/' + programId + '/main">Edit</a>'
+			'<a href="/admin/programs/edit_instructions/1">Edit</a>'
 		],
 		[
 			'Program Media Instructions', 
-			'<a href="/admin/programs/edit_instructions/' + programId + '/media">Edit</a>'
+			'<a href="/admin/programs/edit_instructions/2">Edit</a>'
 		],
 		[
 			'Program Form Instructions', 
-			'<a href="/admin/programs/edit_instructions/' + programId + '/form">Edit</a>'
+			'<a href="/admin/programs/edit_instructions/3">Edit</a>'
 		],
 		[
 			'Program Document Instructions', 
-			'<a href="/admin/programs/edit_instructions/' + programId + '/document">Edit</a>'
+			'<a href="/admin/programs/edit_instructions/4">Edit</a>'
 		],
 		[
 			'Program E-Sign Instructions', 
-			'<a href="/admin/programs/edit_instructions/' + programId + '/esign">Edit</a>'
+			'<a href="/admin/programs/edit_instructions/5">Edit</a>'
 		]		
 		
 	];
