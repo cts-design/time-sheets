91b2ba6a54efaff6a563a84d140b1d74e9f4834e
Daniel Nolan <daniel@ctsfla.com>
2011-05-26 16:23:13 -0400

    changing the js file to apply the EXTjs htmlEditor to an existng form textarea

--- webroot/js/programs/admin_edit_instructions.js
+++ webroot/js/programs/admin_edit_instructions.js
@@ -1,33 +1,9 @@
 Ext.onReady(function(){
 	Ext.QuickTips.init();
-	
-	var editForm = new Ext.form.FormPanel({
-		renderTo: 'editForm',
-		title: 'Edit Program Instructions',
-		height: 300,
-		width: 515,
-		layout: 'fit',
-		frame: true,
-		items: [{
-			id: 'htmlEditor',
-			name: 'data[ProgramInstruction][text]',
-			xtype: 'htmleditor'
-		}],
-		buttons: [{
-			text: 'Submit',
-			handler: function() {
-				var form = editForm.getForm();
-				var vals = form.getFieldValues();
-				form.submit({
-					url: window.location,
-					success: function(form, action){
-						Ext.Msg.alert('Success', action.result.message);
-					},
-					failure: function(form, action) {
-						Ext.Msg.alert('Failure', action.result.message);
-					}
-				});
-			}
-		}]
+	var editor = new Ext.form.HtmlEditor({
+		width: 600,
+		height: 600
 	});
+	console.log(editor);
+	editor.applyToMarkup('ProgramInstructionText');
 });	
