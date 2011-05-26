3d79e1bf54cce1550d13630d3706e0425df1cb0d
Daniel Nolan <daniel@ctsfla.com>
2011-05-26 14:01:53 -0400

    adding custom validation methods to validate uploaded documents
    time: 1h15m
    re #75

--- models/queued_document.php
+++ models/queued_document.php
@@ -27,75 +27,87 @@ class QueuedDocument extends AppModel {
 	    'foreignKey' => 'self_scan_cat_id'
 	),
     );
+	
+	var $validate = array(
+		'submittedfile' => array(
+			'pdf' => array(
+				'rule' =>'isPDF',
+				'message' => 'Please supply document in .pdf format.'
+			),
+			'lessThen1mb' => array(
+				'rule' => 'lessThen1mb',
+				'message' => 'Document must not be larger then 1mb'
+			)
+		)
+	);
 
     function  beforeDelete($cascade = true) {
-	parent::beforeDelete($cascade);
-	if(!empty($this->data['QueuedDocument'])) {
-	    $adminId = $this->data['QueuedDocument']['last_activity_admin_id'];
-	    $reason = $this->data['QueuedDocument']['reason'];
-	    $deletedLocation = $this->data['QueuedDocument']['deleted_location_id'];
-	}
-	$delDoc = ClassRegistry::init('DeletedDocument');
-	$this->recursive = -1;	
-	$doc = $this->read(null, $this->id);
-	foreach($doc as $k => $v) {
-	    $this->data['DeletedDocument'] = $v;
-	    if(isset($adminId, $reason, $deletedLocation)) {
-		$this->data['DeletedDocument']['last_activity_admin_id'] = $adminId;
-		$this->data['DeletedDocument']['deleted_reason'] = $reason;
-		$this->data['DeletedDocument']['deleted_location_id'] = $deletedLocation;
-	    }
-	    unset($this->data['DeletedDocument']['modified']);
-	    unset($this->data['QueuedDocument']);
-	}
-	if(!empty($adminId) && !empty($reason)) {
-	   if($delDoc->save($this->data)) {
-		return true;
-	    }
-	    else return false;
-	}
-	else {
-	    return true;
-	}
+		parent::beforeDelete($cascade);
+		if(!empty($this->data['QueuedDocument'])) {
+		    $adminId = $this->data['QueuedDocument']['last_activity_admin_id'];
+		    $reason = $this->data['QueuedDocument']['reason'];
+		    $deletedLocation = $this->data['QueuedDocument']['deleted_location_id'];
+		}
+		$delDoc = ClassRegistry::init('DeletedDocument');
+		$this->recursive = -1;	
+		$doc = $this->read(null, $this->id);
+		foreach($doc as $k => $v) {
+		    $this->data['DeletedDocument'] = $v;
+		    if(isset($adminId, $reason, $deletedLocation)) {
+				$this->data['DeletedDocument']['last_activity_admin_id'] = $adminId;
+				$this->data['DeletedDocument']['deleted_reason'] = $reason;
+				$this->data['DeletedDocument']['deleted_location_id'] = $deletedLocation;
+		    }
+		    unset($this->data['DeletedDocument']['modified']);
+		    unset($this->data['QueuedDocument']);
+		}
+		if(!empty($adminId) && !empty($reason)) {
+		   if($delDoc->save($this->data)) {
+				return true;
+		    }
+		    else return false;
+		}
+		else {
+		    return true;
+		}
     }
 
     function checkLocked($userId) {
-	$lockedConditions['QueuedDocument.locked_status'] = 1;
-	$lockedConditions['QueuedDocument.locked_by'] = $userId;
-	$userLockedDoc = $this->find('first', array('conditions' => $lockedConditions));
-	if (!empty($userLockedDoc['QueuedDocument']['id'])) {
-	    if (!$this->unlockDocument($userLockedDoc['QueuedDocument']['id'])) {
-		return false;
-	    }
-	    else return true;
-	}
-	else return false;
+		$lockedConditions['QueuedDocument.locked_status'] = 1;
+		$lockedConditions['QueuedDocument.locked_by'] = $userId;
+		$userLockedDoc = $this->find('first', array('conditions' => $lockedConditions));
+		if(!empty($userLockedDoc['QueuedDocument']['id'])) {
+		    if (!$this->unlockDocument($userLockedDoc['QueuedDocument']['id'])) {
+				return false;
+		    }
+		    else return true;
+		}
+		else return false;
     }
 
     function lockDocument($id=null, $userId=null) {
-	if ($id && $userId) {
-	    $this->data['QueuedDocument']['id'] = $id;
-	    $this->data['QueuedDocument']['locked_by'] = $userId;
-	    $this->data['QueuedDocument']['locked_status'] = 1;
-	    if ($this->save($this->data)) {
-		return true;
-	    }
-	    else
-		return false;
-	}
+		if($id && $userId) {
+		    $this->data['QueuedDocument']['id'] = $id;
+		    $this->data['QueuedDocument']['locked_by'] = $userId;
+		    $this->data['QueuedDocument']['locked_status'] = 1;
+		    if($this->save($this->data)) {
+				return true;
+		    }
+		    else
+			return false;
+		}
     }
 
     function unlockDocument($id=null) {
-	if ($id) {
-	    $this->data['QueuedDocument']['id'] = $id;
-	    $this->data['QueuedDocument']['locked_by'] = null;
-	    $this->data['QueuedDocument']['locked_status'] = 0;
-	    if ($this->save($this->data)) {
-		return true;
-	    }
-	    else
-		return false;
-	}
+		if($id) {
+		    $this->data['QueuedDocument']['id'] = $id;
+		    $this->data['QueuedDocument']['locked_by'] = null;
+		    $this->data['QueuedDocument']['locked_status'] = 0;
+		    if($this->save($this->data)) {
+				return true;
+		    }
+		    else return false;
+		}
     }
 
 	function uploadDocument($data, $entryMethod, $id){
@@ -135,6 +147,20 @@ class QueuedDocument extends AppModel {
 		else {
 		    return false;
 		}
+	}
+
+	function isPDF() {
+		if($this->data['QueuedDocument']['submittedfile']['type'] != 'application/pdf') {
+			return false;
+		}
+		return true;
+	}
+	
+	function lessThen1mb() {
+		if($this->data['QueuedDocument']['submittedfile']['size'] > 1048576) {
+			return false;
+		}
+		return true;		
 	}
 
 }
