52a0c1dd80cabe8566dcef17aaf2d9816da78fb0
Daniel Nolan <daniel@ctsfla.com>
2011-06-06 14:40:07 -0400

    adding test methods for view_media
    time: 2h39m
    
    re #75

--- tests/cases/controllers/program_controllers/programs_controller.test.php
+++ tests/cases/controllers/program_controllers/programs_controller.test.php
@@ -2,43 +2,33 @@
 /* Programs Test cases generated on: 2011-03-29 12:34:28 : 1301402068*/
 App::import('Controller', 'Programs');
 App::import('Lib', 'AtlasTestCase');
+
+App::import('Component', 'Email');
+Mock::generate('EmailComponent');
+
 class TestProgramsController extends ProgramsController {
 	
 	public $name = 'Programs';
 	
 	public $autoRender = false;
 	
-	public $testView = false;
-
 	public function redirect($url, $status = null, $exit = true) {
 		$this->redirectUrl = $url;
-	}
-	
-    public function render($action = null, $layout = null, $file = null) {
-        if (!$this->testView) {
-            $this->renderedAction = $action;
-        } else {
-            return parent::render($action, $layout, $file);
-        }
-    }
-
-  	public function _stop($status = 0) {
-        $this->stopped = $status;
-    }	
+	}	
 }
 
 class ProgramsControllerTestCase extends AtlasTestCase {
 	
-	function startTest() {
+	public function startTest() {
 		$this->Programs =& new TestProgramsController();
-		$this->Programs->constructClasses();	
+		$this->Programs->constructClasses();
         $this->Programs->params['controller'] = 'programs';
         $this->Programs->params['pass'] = array();
         $this->Programs->params['named'] = array();	
 		$this->testController = $this->Programs;
 	}
 	
-	function testIndexNoResponse() {
+	public function testIndexNoResponse() {
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
 	        'id' => 10,
@@ -71,7 +61,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 	}	
 	
 	
-	function testIndexWithResponse() {
+	public function testIndexWithResponse() {
 		$this->Programs->params['action'] = 'index';
         $this->Programs->params['url']['url'] = 'index/1';
 		$this->Programs->Component->initialize($this->Programs);		
@@ -85,10 +75,11 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 	    $this->Programs->Component->startup($this->Programs);
 		$this->Programs->index(1);
 		$expectedResult = array('controller' => 'programs', 'action' => 'view_media', 0 => 1);
-		$this->assertEqual($this->Programs->redirectUrl, $expectedResult );		
+		$this->assertEqual($this->Programs->redirectUrl, $expectedResult );
+		$this->Programs->Session->destroy();		
 	}
 	
-	function testIndexDisabledProgram() {
+	public function testIndexDisabledProgram() {
 		$this->Programs->params = Router::parse('/programs/index/2');
 		
 		$this->Programs->Component->initialize($this->Programs);
@@ -105,7 +96,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->Session->destroy();	
 	}
 	
-	function testIndexNoId() {
+	public function testIndexNoId() {
 		$this->Programs->params = Router::parse('/programs/index');
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -122,7 +113,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->Session->destroy();			
 	}
 	
-	function testIndexViewedMedia() {
+	public function testIndexViewedMedia() {
 		$this->Programs->params = Router::parse('/programs/index/1');
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -139,7 +130,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->Session->destroy();			
 	}
 	
-	function testIndexCompletedForm() {
+	public function testIndexCompletedForm() {
 		$this->Programs->params = Router::parse('/programs/index/1');
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -156,7 +147,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->Session->destroy();		
 	}
 	
-	function testIndexUploadedDocs() {
+	public function testIndexUploadedDocs() {
 		$this->Programs->params = Router::parse('/programs/index/1');			
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -177,7 +168,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->Session->destroy();		
 	}
 	
-	function testIndexDroppingOffDocs() {
+	public function testIndexDroppingOffDocs() {
 		$this->Programs->params = Router::parse('/programs/index/1');	
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -198,7 +189,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->Session->destroy();		
 	}
 	
-	function testIndexResponseComplete() {
+	public function testIndexResponseComplete() {
 		$this->Programs->params = Router::parse('/programs/index/1');	
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -218,7 +209,7 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->Session->destroy();		
 	}
 
-	function testGetStarted() {
+	public function testGetStarted() {
 		$this->Programs->params = Router::parse('/programs/get_started');
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -246,11 +237,10 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->assertEqual($result['ProgramResponse']['id'], 7);
 		$this->assertEqual($result['ProgramResponse']['user_id'], 10);
 		$this->assertEqual($this->Programs->redirectUrl, '/programs/view_media/1');
-		$this->Programs->Session->destroy();	
-		
+		$this->Programs->Session->destroy();		
 	}
 	
-	function testViewMediaNoId() {		
+	public function testViewMediaNoId() {		
 		$this->Programs->params = Router::parse('/programs/view_media');
 		$this->Programs->Component->initialize($this->Programs);
 		$this->Programs->Session->write('Auth.User', array(
@@ -262,11 +252,74 @@ class ProgramsControllerTestCase extends AtlasTestCase {
 		$this->Programs->beforeFilter();		
 	    $this->Programs->Component->startup($this->Programs);			
 		$this->Programs->view_media();
-		$this->assertEqual($this->Programs->Session->read('Message.flash.element'), 'flash_failure');	
+		$this->assertEqual($this->Programs->Session->read('Message.flash.element'), 'flash_failure');
+		$this->Programs->Session->destroy();	
+	}
+	
+	public function testViewMedia()	{
+		$this->Programs->params = Router::parse('/programs/view_media/1');
+		$this->Programs->Component->initialize($this->Programs);
+		$this->Programs->data = array(
+			'ProgramResponse' => array(
+				'viewed_media' => 1,
+				'program_id' => 1
+			)
+		);
+		$this->Programs->Session->write('Auth.User', array(
+	        'id' => 9,
+	        'role_id' => 1,
+	        'username' => 'smith'
+	    ));
+		$this->Programs->Email =& new MockEmailComponent();
+	    $this->Programs->Email->setReturnValue('send', true);
+		$this->Programs->Email->enabled = true;
+		$this->Programs->Session->write('step2', 'form');
+		$this->mockAcl($this->Programs);		
+		$this->Programs->beforeFilter();		
+	    $this->Programs->Component->startup($this->Programs);			
+		$this->Programs->view_media(1);
+		
+		$result = $this->Programs->Program->ProgramResponse->find('first', array('conditions' => array(
+			'ProgramResponse.user_id' => 9,
+			'ProgramResponse.program_id' => 1,
+			'ProgramResponse.expires_on >=' => date('Y-m-d H:i:s', strtotime('6/1/11'))
+		)));
+		$redirect = array(
+			'controller' => 'program_responses',
+			'action' => 'index',
+			0 => 1
+		);
+		$this->assertEqual($this->Programs->redirectUrl, $redirect);
+		$this->assertEqual($result['ProgramResponse']['viewed_media'], 1);
+		$this->assertEqual($result['ProgramResponse']['user_id'], 9);
+		$this->assertEqual($result['ProgramResponse']['program_id'], 1);	
+		$this->Programs->Session->destroy();		
+	}
+
+	public function testViewMediaBadData()	{
+		$this->Programs->params = Router::parse('/programs/view_media/1');
+		$this->Programs->Component->initialize($this->Programs);
+		$this->Programs->data = array(
+			'ProgramResponse' => array(
+				'viewed_media' => '',
+				'program_id' => 1
+			)
+		);
+		$this->Programs->Session->write('Auth.User', array(
+	        'id' => 9,
+	        'role_id' => 1,
+	        'username' => 'smith'
+	    ));
+		$this->mockAcl($this->Programs);		
+		$this->Programs->beforeFilter();		
+	    $this->Programs->Component->startup($this->Programs);			
+		$this->Programs->view_media(1);
+		
+		$this->assertEqual($this->Programs->Session->read('Message.flash.element'), 'flash_failure');		
+		$this->Programs->Session->destroy();		
 	}	
 	
-	function endTest() {
-		//$this->Programs->Session->destroy();
+	public function endTest() {
 		unset($this->Programs);
 		ClassRegistry::flush();
 	}	
