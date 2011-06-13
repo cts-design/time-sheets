f2809b0ce014778380ee333472a9a981b7d25abe
Daniel Nolan <daniel@ctsfla.com>
2011-06-13 16:56:56 -0400

    updating fixture with test data

--- tests/fixtures/watched_filing_cat_fixture.php
+++ tests/fixtures/watched_filing_cat_fixture.php
@@ -1,5 +1,5 @@
 <?php
-/* WatchedFilingCat Fixture generated on: 2011-04-06 15:17:53 : 1302103073 */
+/* WatchedFilingCat Fixture generated on: 2011-06-13 14:46:57 : 1307990817 */
 class WatchedFilingCatFixture extends CakeTestFixture {
 	var $name = 'WatchedFilingCat';
 
@@ -8,15 +8,24 @@ class WatchedFilingCatFixture extends CakeTestFixture {
 		'cat_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
 		'program_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'cat_id' => array('column' => 'cat_id', 'unique' => 0)),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
 
 	var $records = array(
 		array(
 			'id' => 1,
-			'cat_id' => 1,
+			'cat_id' => 253,
+			'program_id' => 1
+		),
+		array(
+			'id' => 2,
+			'cat_id' => 252,
+			'program_id' => 1
+		),
+		array(
+			'id' => 3,
+			'cat_id' => 254,
 			'program_id' => 1
 		),
 	);
 }
-?>
