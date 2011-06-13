34988e535f8982fc083deafdc147565fda32fce9
Daniel Nolan <daniel@ctsfla.com>
2011-06-13 16:56:32 -0400

    updating fixture with test data

--- tests/fixtures/location_fixture.php
+++ tests/fixtures/location_fixture.php
@@ -1,20 +1,63 @@
 <?php
-/* Location Fixture generated on: 2010-11-05 19:11:13 : 1288983613 */
+/* Location Fixture generated on: 2011-06-13 15:52:52 : 1307994772 */
 class LocationFixture extends CakeTestFixture {
 	var $name = 'Location';
 
 	var $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'public_name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'address_1' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'address_2' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'city' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'state' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 2, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'zip' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 5, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'country' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'telephone' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 14, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'fax' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 14, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
 
 	var $records = array(
 		array(
 			'id' => 1,
-			'name' => 'Lorem ipsum dolor sit amet'
+			'name' => 'Citrus',
+			'public_name' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'city' => '',
+			'state' => '',
+			'zip' => '',
+			'country' => '',
+			'telephone' => '',
+			'fax' => ''
+		),
+		array(
+			'id' => 2,
+			'name' => 'Levy',
+			'public_name' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'city' => '',
+			'state' => '',
+			'zip' => '',
+			'country' => '',
+			'telephone' => '',
+			'fax' => ''
+		),
+		array(
+			'id' => 3,
+			'name' => 'Paddock',
+			'public_name' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'city' => '',
+			'state' => '',
+			'zip' => '',
+			'country' => '',
+			'telephone' => '',
+			'fax' => ''
 		),
 	);
 }
-?>
