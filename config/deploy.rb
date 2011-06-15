75e68dee3204324ae6834ccc54b4db48a391c831
Brandon Cordell <brandonc@ctsfla.com>
2011-06-15 10:12:33 -0400

    Changed the deployment info and the webroot/index.php info that was overwritten while merging cccp_staging back into staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,16 +4,16 @@ require 'capcake'
 set :application, 'atlas' # Your app's location (domain or sub-domain name)
 set :repository, "git@git.assembla.com:CTSATLAS.git"
 
-set :branch, 'cccp_staging'
+set :branch, 'staging'
 set :deploy_via, :export
 
 set :default_shell, '/bin/bash'
 
-set :deploy_to, "/var/www/vhosts/ccc.atlasforworkforce.com/atlas"
+set :deploy_to, "/var/www/vhosts/development.ctsfla.com/atlas"
 
-server "ccc.atlasforworkforce.com", :app, :web, :db, :primary => true
+server "development.ctsfla.com", :app, :web, :db, :primary => true
 
-set :user, 'b78ghfp6y'
+set :user, 'dev4cts'
 
 # Cake Settings
 set :cake_branch, "master"
