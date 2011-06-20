638e6f378634eebf6c15dc8cb90a6c0a7a19a52f
Brandon Cordell <brandonc@ctsfla.com>
2011-06-20 08:58:54 -0400

    Added tests to the navigations controller tests to make sure that links created with no leading slash have the leading slash appended before save and that links with http(s) at the beginning have no forward slash appended to them
    re #118
    Time: 6h32m

--- tests/cases/controllers/navigations_controller.test.php
+++ tests/cases/controllers/navigations_controller.test.php
@@ -2,6 +2,7 @@
 /* Navigations Test cases generated on: 2011-02-04 19:52:09 : 1296849129*/
 App::import('Controller', 'Navigations');
 App::import('Lib', 'AtlasTestCase');
+App::import('Vendor', 'DebugKit.FireCake');
 class TestNavigationsController extends NavigationsController {
 	var $autoRender = false;
 
@@ -14,6 +15,7 @@ class NavigationsControllerTestCase extends AtlasTestCase {
 	function startTest() {
 		$this->Navigations =& new TestNavigationsController();
 		$this->Navigations->constructClasses();
+        $this->testController = $this->Navigations;
 	}
 
 	function endTest() {
@@ -29,12 +31,157 @@ class NavigationsControllerTestCase extends AtlasTestCase {
 
 	}
 
-	function testAdminAdd() {
+	function testAdminCreateInternalUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/create');
+        $this->Navigations->params['form'] = array(
+            'name' => 'My New Link',
+            'link' => 'my_new_link',
+            'parentId' => 19
+        );
 
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_create(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'My New Link');
+        $this->assertEqual($result['navigation']['link'], '/my_new_link');
 	}
 
-	function testAdminEdit() {
+	function testAdminCreateExternalUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/create');
+        $this->Navigations->params['form'] = array(
+            'name' => 'Google',
+            'link' => 'http://google.com',
+            'parentId' => 19
+        );
 
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_create(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'Google');
+        $this->assertEqual($result['navigation']['link'], 'http://google.com');
+	}
+
+	function testAdminCreateSecureExternalUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/create');
+        $this->Navigations->params['form'] = array(
+            'name' => 'Google',
+            'link' => 'https://google.com',
+            'parentId' => 19
+        );
+
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_create(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'Google');
+        $this->assertEqual($result['navigation']['link'], 'https://google.com');
+	}
+
+	function testAdminCreateInternalModuleUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/create');
+        $this->Navigations->params['form'] = array(
+            'name' => 'Helpful Articles',
+            'link' => '/pages/helpful_articles',
+            'parentId' => 19
+        );
+
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_create(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'Helpful Articles');
+        $this->assertEqual($result['navigation']['link'], '/pages/helpful_articles');
+	}
+
+
+	function testAdminUpdateInternalUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/update');
+        $this->Navigations->params['form'] = array(
+            'name' => 'My New Link',
+            'link' => 'my_new_link',
+            'parentId' => 19
+        );
+
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_update(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'My New Link');
+        $this->assertEqual($result['navigation']['link'], '/my_new_link');
+	}
+
+	function testAdminUpdateExternalUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/update');
+        $this->Navigations->params['form'] = array(
+            'name' => 'Google',
+            'link' => 'http://google.com',
+            'parentId' => 19
+        );
+
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_update(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'Google');
+        $this->assertEqual($result['navigation']['link'], 'http://google.com');
+	}
+
+	function testAdminUpdateSecureExternalUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/update');
+        $this->Navigations->params['form'] = array(
+            'name' => 'Google',
+            'link' => 'https://google.com',
+            'parentId' => 19
+        );
+
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_update(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'Google');
+        $this->assertEqual($result['navigation']['link'], 'https://google.com');
+	}
+
+	function testAdminUpdateInternalModuleUrl() {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->Navigations->params = Router::parse('/admin/navigations/update');
+        $this->Navigations->params['form'] = array(
+            'name' => 'Helpful Articles',
+            'link' => '/pages/helpful_articles',
+            'parentId' => 19
+        );
+
+        $this->Navigations->Component->initialize($this->Navigations);
+        $this->Navigations->beforeFilter();
+        $this->Navigations->Component->startup($this->Navigations);
+
+        $result = json_decode($this->Navigations->admin_update(), true);
+        $this->assertTrue($result['success']);
+        $this->assertEqual($result['navigation']['title'], 'Helpful Articles');
+        $this->assertEqual($result['navigation']['link'], '/pages/helpful_articles');
 	}
 
 	function testAdminDelete() {
