0f659e1b431ecfdaca78f48c93f3b8a5650a147d
Daniel Nolan <daniel@ctsfla.com>
2011-06-21 16:33:16 -0400

    adding logic to beforeFilter method to allow logged in admins to access release notes page without having to have permissions in ACL
    time: 0h10m
    
    re #126

--- controllers/release_notes_controller.php
+++ controllers/release_notes_controller.php
@@ -4,6 +4,13 @@ class ReleaseNotesController extends AppController {
 	var $name = 'ReleaseNotes';
 	
 	var $uses = array();
+	
+	function beforeFilter() {
+		parent::beforeFilter();
+		if($this->Auth->user('role_id') > 1) {
+			$this->Auth->allowedActions = array('admin_index');
+		}
+	}
 
 	function admin_index() {
 		$title_for_layout = 'Atlas Release Notes';
