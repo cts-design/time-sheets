14a803cd9d3e27bb82e1b28d6573eb82617f384a
Brandon Cordell <brandonc@ctsfla.com>
2011-07-29 13:11:42 -0400

    Added the translate behaviors to the FeaturedEmployer model
    re #148
    Time: 0h3m

--- models/featured_employer.php
+++ models/featured_employer.php
@@ -2,5 +2,13 @@
 class FeaturedEmployer extends AppModel {
 	var $name = 'FeaturedEmployer';
 	var $displayField = 'name';
+	var $actsAs = array(
+		'Translatable' => array(
+			'description'
+		),
+		'AtlasTranslate' => array(
+			'description'
+		)
+	);
 }
 ?>
