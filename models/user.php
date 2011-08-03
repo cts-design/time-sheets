fd636e6575c71554a7e1250ca926f12fbd10d8f0
Daniel Nolan <daniel@ctsfla.com>
2011-08-03 15:31:16 -0400

    fixing warning in user model caused by invalid argument in a foreach loop. wrapped foreach in a conditional statement to check if $results is an array if it is we loop through it, otherwise we just return $results

--- models/user.php
+++ models/user.php
@@ -842,16 +842,18 @@ class User extends AppModel {
    
     function  afterFind($results, $primary = false) {
 		parent::afterFind($results, $primary);
-		foreach ($results as $key => $value) {
-		    if(isset($value['User']['created'])) {
-				$results[$key]['User']['created'] = $this->formatDateTimeAfterFind($value['User']['created']);
-		    }
-		    if(isset($value['User']['modified'])) {
-				$results[$key]['User']['modified'] = $this->formatDateTimeAfterFind($value['User']['modified']);
-		    }
-		    if(isset($value['User']['dob'])) {
-				$results[$key]['User']['dob'] = $this->formatDateAfterFind($value['User']['dob']);
-		    }
+		if(is_array($results)) {
+			foreach ($results as $key => $value) {
+			    if(isset($value['User']['created'])) {
+					$results[$key]['User']['created'] = $this->formatDateTimeAfterFind($value['User']['created']);
+			    }
+			    if(isset($value['User']['modified'])) {
+					$results[$key]['User']['modified'] = $this->formatDateTimeAfterFind($value['User']['modified']);
+			    }
+			    if(isset($value['User']['dob'])) {
+					$results[$key]['User']['dob'] = $this->formatDateAfterFind($value['User']['dob']);
+			    }
+			}			
 		}
 		return $results;
     }
