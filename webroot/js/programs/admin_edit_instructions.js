7452a5f5c0542cc6671801df9af75f9d51145b7b
Daniel Nolan <daniel@ctsfla.com>
2011-08-16 09:33:04 -0400

    removing webroot/js/programs/admin_edit_instructions.js
    re #14

--- webroot/js/programs/admin_edit_instructions.js
+++
@@ -1,9 +0,0 @@
-Ext.onReady(function(){
-	Ext.QuickTips.init();
-	var editor = new Ext.form.HtmlEditor({
-		width: 600,
-		height: 600
-	});
-	console.log(editor);
-	editor.applyToMarkup('ProgramInstructionText');
-});	
