5de13d0e5ec4ba0c5a5eefbf96a9ba2279d7be10
Daniel Nolan <daniel@ctsfla.com>
2011-08-24 14:27:59 -0400

    adding getProgramResponse method this method will return the programResponse for a user based off programId and userId
    re #14
    time: 1h10m

--- models/program_models/program_response.php
+++ models/program_models/program_response.php
@@ -10,5 +10,24 @@ class ProgramResponse extends AppModel {
 	
 	var $validate = array();
 	
-	
+	function getProgramResponse($programId, $userId) {
+		$this->recursive = -1;
+		$programResponses = $this->find('all', array(
+			'conditions' => array(
+				'ProgramResponse.user_id' => $userId,
+				'ProgramResponse.program_id' => $programId)));
+		$date = date('Y-m-d H:i:s');
+		$completedResponse = Set::extract('/ProgramResponse[complete=1]', $programResponses);
+		$openResponse = Set::extract("/ProgramResponse[expires_on>$date]", $programResponses);
+		if(!empty($completedResponse)) {
+			$programResponse = $completedResponse[0];
+		}
+		elseif(!empty($openResponse)) {
+			$programResponse = $openResponse[0];
+		}
+		else {
+			$programResponse = null;
+		}
+		return $programResponse;		
+	}
 }
