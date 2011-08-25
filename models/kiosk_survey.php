785f7312a953117467216676b1400fea34d4b548
Brandon Cordell <brandonc@ctsfla.com>
2011-08-25 14:05:42 -0400

    Commiting kiosk survey model and kiosk questions survey model
    re #192
    time: 0h45m

---
+++ models/kiosk_survey.php
@@ -0,0 +1,12 @@
+<?php
+class KioskSurvey extends AppModel {
+	var $name = 'KioskSurvey';
+    var $displayField = 'name';
+    var $hasMany = array(
+      'KioskSurveyQuestion' => array(
+        'dependent' => true
+      )
+    );
+    var $belongsTo = array('Kiosk');
+}
+?>
