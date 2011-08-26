e002facb9590288ca8c216e51149d56599f4d091
Brandon Cordell <brandonc@ctsfla.com>
2011-08-26 11:29:44 -0400

    Removed the default admin scaffolding from the KioskSurveysController
    re #192
    time: 0h2m

--- controllers/kiosk_surveys_controller.php
+++ controllers/kiosk_surveys_controller.php
@@ -58,53 +58,5 @@ class KioskSurveysController extends AppController {
 	  $this->set('data', $data);
 	  return $this->render(null, null, '/elements/ajaxreturn');  
 	}
-	
-	function admin_index() {
-		$this->KioskSurvey->recursive = 0;
-		$this->set('kioskSurveys', $this->paginate());
-	}
-
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->KioskSurvey->create();
-			if ($this->KioskSurvey->save($this->data)) {
-				$this->Session->setFlash(__('The kiosk survey has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The kiosk survey could not be saved. Please, try again.', true), 'flash_failure');
-			}
-		}
-	}
-
-	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid kiosk survey', true), 'flash_failure');
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->KioskSurvey->save($this->data)) {
-				$this->Session->setFlash(__('The kiosk survey has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The kiosk survey could not be saved. Please, try again.', true), 'flash_failure');
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->KioskSurvey->read(null, $id);
-		}
-	}
-
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for kiosk survey', true), 'flash_failure');
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->KioskSurvey->delete($id)) {
-			$this->Session->setFlash(__('Kiosk survey deleted', true), 'flash_success');
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Kiosk survey was not deleted', true), 'flash_failure');
-		$this->redirect(array('action' => 'index'));
-	}
 }
-?>
+
