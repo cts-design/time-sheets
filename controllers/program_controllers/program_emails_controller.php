564eaadbb3a69be0b11921fca6c5f44ab99b6b18
Daniel Nolan <daniel@ctsfla.com>
2011-08-29 15:10:42 -0400

    adding admin_edit, admin_index, and admin_toggle_disabled actions to the program_emails controller
    re #14
    
    time: 1h10m

--- controllers/program_controllers/program_emails_controller.php
+++ controllers/program_controllers/program_emails_controller.php
@@ -3,56 +3,85 @@ class ProgramEmailsController extends AppController {
 
 	var $name = 'ProgramEmails';
 
-	function admin_index() {
-		$this->ProgramEmail->recursive = 0;
-		$this->set('programEmails', $this->paginate());
-	}
-
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->ProgramEmail->create();
-			if ($this->ProgramEmail->save($this->data)) {
-				$this->Session->setFlash(__('The program email has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The program email could not be saved. Please, try again.', true), 'flash_failure');
+	function admin_index($id=null) {
+		if(!$id){
+			$this->Session->setFlash(__('Invalid program id.', true), 'flash_failure');
+			$this->redirect(array(
+				'controller' => 'programs', 
+				'action' => 'index', 
+				'admin' => true));
+		}
+		if($this->RequestHandler->isAjax()) {
+			$this->ProgramEmail->recursive = -1;
+			if($id) {
+				$this->ProgramEmail->Behaviors->disable('Disableable');
+				$emails = $this->ProgramEmail->find('all', array(
+					'conditions' => array(
+						'ProgramEmail.program_id' => $id
+					)));			
+				if($emails) {
+					$i = 0;
+					foreach ($emails as $email) {
+						$data['emails'][$i] = $email['ProgramEmail'];
+						if($email['ProgramEmail']['disabled'] == 0) {
+							$data['emails'][$i]['disabled'] = 'No';	
+						}
+						else {
+							$data['emails'][$i]['disabled'] = 'Yes';	
+						}
+						$i++;	
+					}
+				}					
 			}
-		}
-		$programs = $this->ProgramEmail->Program->find('list');
-		$this->set(compact('programs'));
-	}
-
-	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid program email', true), 'flash_failure');
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->ProgramEmail->save($this->data)) {
-				$this->Session->setFlash(__('The program email has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The program email could not be saved. Please, try again.', true), 'flash_failure');
+			else {
+				$data['message'] = 'Invalid program id.';
+				$data['success'] = false;
 			}
+			$this->set('data', $data);
+			$this->render('/elements/ajaxreturn');
 		}
-		if (empty($this->data)) {
-			$this->data = $this->ProgramEmail->read(null, $id);
-		}
-		$programs = $this->ProgramEmail->Program->find('list');
-		$this->set(compact('programs'));
+		$title_for_layout = 'Program Emails';
+		$this->set(compact('title_for_layout'));
 	}
 
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for program email', true), 'flash_failure');
-			$this->redirect(array('action'=>'index'));
+	function admin_edit() {
+		if($this->RequestHandler->isAjax()) {
+			if(!empty($this->data)) {
+				if($this->ProgramEmail->save($this->data)) {
+					$data['success'] = true;
+					$data['message'] = 'Email saved sucessfully.';	
+				}
+				else {
+					$data['success'] = false;
+					$data['message'] = 'Unable to save email.';					
+				}
+			}
+			else {
+				$data['success'] = false;
+				$data['message'] = 'Unable to save email.';				
+			}
+			$this->set('data', $data);
+			$this->render('/elements/ajaxreturn');				
 		}
-		if ($this->ProgramEmail->delete($id)) {
-			$this->Session->setFlash(__('Program email deleted', true), 'flash_success');
-			$this->redirect(array('action'=>'index'));
+	}
+	
+	function admin_toggle_disabled($id, $disabled) {
+		if($this->RequestHandler->isAjax()) {
+			if($this->ProgramEmail->toggleDisabled($id, $disabled)) {
+				$data['success'] = true;
+				if($disabled == 1) {
+					$data['message'] = 'Email disabled successfully.';
+				}
+				else {
+					$data['message'] = 'Email enabled successfully.';
+				} 
+			}
+			else {
+				$data['success'] = false;
+				$data['message'] = 'Unbale to save, please try again.';
+			}
+			$this->set(compact('data'));
+			$this->render(null, null, '/elements/ajaxreturn');
 		}
-		$this->Session->setFlash(__('Program email was not deleted', true), 'flash_failure');
-		$this->redirect(array('action' => 'index'));
 	}
 }
-?>
