f72611d555c443d707a61c18edb3aad5de6721da
Daniel Nolan <daniel@ctsfla.com>
2011-10-13 13:59:06 -0400

    fixing bug where approval buttons were not showing if approval is required

--- webroot/js/program_responses/admin_view.js
+++ webroot/js/program_responses/admin_view.js
@@ -67,131 +67,139 @@ Ext.onReady(function(){
 	    }]
 	});
 
-var menu = Ext.create('Ext.menu.Menu', {
-    layout: 'menu',
-	items: [
-		approvalForm
-	]
-});	
-
-var tb = Ext.create('Ext.toolbar.Toolbar', {
-	hidden: true,
-	buttonAlign: 'center',
-	items: [{
-		text: 'Approved',
-		icon: '/img/icons/accept.png',
-		handler: function() {
-			Ext.Msg.wait('Please wait', 'Status');
-			Ext.Ajax.request({
-				url: '/admin/program_responses/approve/'+programResponseId,
-				success: function(response, opts) {
-					var obj = Ext.decode(response.responseText);
-					if(obj.success) {
-						tb.hide();
-						Ext.Msg.alert('Status', obj.message);						
-					}
-					else {
-						opts.failure(response, opts, obj); 						
-					}
-				},
-				failure: function(repsonse, opts, obj) {
-					var msg = '';
-					if(obj.message) {
-						msg = obj.message
-					}
-					else {
-						msg = "An error has occurred";
-					}
-					Ext.Msg.alert('Status', msg);
-				}
-			});
-		}
-	},{
-		text: 'Not Approved',
-		icon: '/img/icons/delete.png',
-		id: 'notApproved',
-		menu: menu
-	}]
-});
-
-
-
-var programResponsePanel = Ext.create('Ext.panel.Panel', {
-	title: 'Program Response',
-	renderTo: 'ProgramResponsePanel',
-	width: 950,
-	height: 400,
-	layout: 'accordion',
-	layoutConfig: {
-		animate: true,
-	},
-	tbar: tb,
-	items: [{
-		title: 'Customer Info',
-		autoScroll: true,
-		autoLoad: '/admin/program_responses/view/'+programResponseId+'/user'
-	},{
-		title: 'Program Response',
-		autoScroll: true,
-		loader: {
-			url: '/admin/program_responses/view/'+programResponseId+'/answers'
-		},
-		listeners: {
-			beforeexpand: updateResponse = function() {
-				this.getLoader().load();
-				this.removeListener('beforeexpand', updateResponse);
-			}
-		}				
-	},{
-		title: 'Documents',
-		id: 'documents',
-		autoScroll: true,
-		loader: {
-			url: '/admin/program_responses/view/'+programResponseId+'/documents'
-		},		
-		listeners: {
-			beforeexpand: updateDoc = function() {
-				this.getLoader().load();
-				this.getLoader().on('load', function(){
-					Ext.get('ProgramPaperForms').on('click', function(e, t){						
-						t = Ext.get(t);
-						if(t.hasCls('generate') || t.hasCls('regenerate')) {
-							e.preventDefault();
-							Ext.Msg.progress('Status', 'Generating Form');					
-							Ext.Ajax.request({
-								url: t.getAttribute('href'),
-								success: function(response, opts) {
-									var obj = Ext.decode(response.responseText);
-									if(obj.success) {
-										progress = Ext.Msg.updateProgress(1, 'Complete', obj.message);
-										hideProgress.delay(2000);
-										Ext.getCmp('documents').getLoader().load();											
-									}
-									else {
-										opts.failure(response, opts);
-									}
-								},
-								failure: function(response, opts) {
-									var obj = Ext.decode(response.responseText);
-									if(!obj.success) {
-										Ext.Msg.alert('Status', obj.message);
-									}
-									else {
-										Ext.Msg.alert('Status', 'An error has occured');
-									}
-								}
-							})
-						}
-					}) 
-				});
-				this.removeListener('beforeexpand', updateDoc);
-			}
-		}			
-	}]
-});
+	var menu = Ext.create('Ext.menu.Menu', {
+	    layout: 'menu',
+		items: [
+			approvalForm
+		]
+	});	
 	
-if(requiresApproval) {
-	tb.show();
-}
+
+	
+	
+	
+	var programResponsePanel = Ext.create('Ext.panel.Panel', {
+		title: 'Program Response',
+		renderTo: 'ProgramResponsePanel',
+		width: 950,
+		height: 400,
+		dockedItems: [{
+			xtype: 'toolbar',
+			dock: 'top',
+			height: 25,
+			id: 'tB',
+			items: [{
+				text: 'Approved',
+				id: 'approved',
+				hidden: false,
+				icon: '/img/icons/accept.png',
+				handler: function() {
+					Ext.Msg.wait('Please wait', 'Status');
+					Ext.Ajax.request({
+						url: '/admin/program_responses/approve/'+programResponseId,
+						success: function(response, opts) {
+							var obj = Ext.decode(response.responseText);
+							if(obj.success) {
+								tb.hide();
+								Ext.Msg.alert('Status', obj.message);						
+							}
+							else {
+								opts.failure(response, opts, obj); 						
+							}
+						},
+						failure: function(repsonse, opts, obj) {
+							var msg = '';
+							if(obj.message) {
+								msg = obj.message
+							}
+							else {
+								msg = "An error has occurred";
+							}
+							Ext.Msg.alert('Status', msg);
+						}
+					});
+				}
+			},{
+				text: 'Not Approved',
+				id: 'notApproved',
+				hidden: false,				
+				icon: '/img/icons/delete.png',
+				id: 'notApproved',
+				menu: menu
+			}]
+		}],
+		layout: 'accordion',
+		layoutConfig: {
+			animate: true,
+		},
+		
+		items: [{
+			title: 'Customer Info',
+			autoScroll: true,
+			autoLoad: '/admin/program_responses/view/'+programResponseId+'/user'
+		},{
+			title: 'Program Response',
+			autoScroll: true,
+			loader: {
+				url: '/admin/program_responses/view/'+programResponseId+'/answers'
+			},
+			listeners: {
+				beforeexpand: updateResponse = function() {
+					this.getLoader().load();
+					this.removeListener('beforeexpand', updateResponse);
+				}
+			}				
+		},{
+			title: 'Documents',
+			id: 'documents',
+			autoScroll: true,
+			loader: {
+				url: '/admin/program_responses/view/'+programResponseId+'/documents'
+			},		
+			listeners: {
+				beforeexpand: updateDoc = function() {
+					this.getLoader().load();
+					this.getLoader().on('load', function(){
+						Ext.get('ProgramPaperForms').on('click', function(e, t){						
+							t = Ext.get(t);
+							if(t.hasCls('generate') || t.hasCls('regenerate')) {
+								e.preventDefault();
+								Ext.Msg.progress('Status', 'Generating Form');					
+								Ext.Ajax.request({
+									url: t.getAttribute('href'),
+									success: function(response, opts) {
+										var obj = Ext.decode(response.responseText);
+										if(obj.success) {
+											progress = Ext.Msg.updateProgress(1, 'Complete', obj.message);
+											hideProgress.delay(2000);
+											Ext.getCmp('documents').getLoader().load();											
+										}
+										else {
+											opts.failure(response, opts);
+										}
+									},
+									failure: function(response, opts) {
+										var obj = Ext.decode(response.responseText);
+										if(!obj.success) {
+											Ext.Msg.alert('Status', obj.message);
+										}
+										else {
+											Ext.Msg.alert('Status', 'An error has occured');
+										}
+									}
+								})
+							}
+						}) 
+					});
+					this.removeListener('beforeexpand', updateDoc);
+				}
+			}			
+		}]
+	});
+	if(!requiresApproval) {
+		Ext.getCmp('approved').hide();
+		Ext.getCmp('notApproved').hide();
+	}
+
 
 });
