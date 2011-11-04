3416e2c2f06d0a01a5a44ff1925aaf3b15bc8213
Daniel Nolan <daniel@ctsfla.com>
2011-11-04 15:12:22 -0400

    adding logic to disable submit buttons on form submit to prevent double form submissions.
    also added #UserSsn1, #UserSsn2, #UserSsn3, #UserSsn1Confirm, #UserSsn2Confirm, #UserSsn3Confirm
    to dPassword so they will be obscured
    
    re #255
    re #224

--- webroot/js/users/mini.registration.js
+++ webroot/js/users/mini.registration.js
@@ -1,10 +1,16 @@
 /* 
  * @author Daniel Nolan
- * @copyright Complete Technology Solutions 2010
+ * @copyright Complete Technology Solutions 2011
  * @link http://ctsfla.com
  */
 
-$(document).ready( function() {
-    $('#UserSsn').dPassword();
-    $('#UserSsnConfirm').dPassword();
+
+$(document).ready(function() {
+	$('#UserSsn, #UserSsnConfirm, #UserSsn1, #UserSsn2, #UserSsn3, #UserSsn1Confirm, #UserSsn2Confirm, #UserSsn3Confirm ').dPassword();
+	$('#UserKioskMiniRegistrationForm').submit(function() {
+		$('.self-sign-kiosk-button, .self-sign-kiosk-link').button({
+			disabled : true
+		});
+		return true;
+	});
 });
