c9c5ce6cb1a1784f36aab1f1666f6088bd5a74e2
Daniel Nolan <daniel@ctsfla.com>
2011-12-08 16:57:52 -0500

    adding add_self_sign_alert action to alerts controller
    re #299

--- controllers/alerts_controller.php
+++ controllers/alerts_controller.php
@@ -4,22 +4,39 @@ class AlertsController extends AppController {
 	var $name = 'Alerts';
 
 	function admin_index() {
-		$this->Alert->recursive = 0;
-		$this->set('alerts', $this->paginate());
 	}
 
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->Alert->create();
-			if ($this->Alert->save($this->data)) {
-				$this->Session->setFlash(__('The alert has been saved', true), 'flash_success');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The alert could not be saved. Please, try again.', true), 'flash_failure');
+	function admin_add_self_sign_alert() {
+		if($this->RequestHandler->isAjax())	{
+			//TODO add security check to make sure a admin or the logged in user is adding the alert
+			ChromePhp::log($this->params);
+			$this->data['Alert']['name'] = $this->params['form']['name'];
+			$this->data['Alert']['type'] = 'selfSign';
+			$this->data['Alert']['user_id'] = $this->params['form']['user_id'];
+			$this->data['Alert']['location_id'] = $this->params['form']['location'];
+			if(isset($this->params['form']['send_email'])) {
+				$this->data['Alert']['send_email'] = 1;
 			}
+			if(!isset($this->params['form']['level2']) && !isset($this->params['form']['level3'])) {
+				$this->data['Alert']['watched_id'] = $this->params['form']['level1'];
+			}
+			elseif(isset($this->params['form']['level2']) && !isset($this->params['form']['level3'])) {
+				$this->data['Alert']['watched_id'] = $this->params['form']['level2'];
+			}
+			elseif(isset($this->params['form']['level2']) && isset($this->params['form']['level3'])) {
+				$this->data['Alert']['watched_id'] = $this->params['form']['level2'];
+			}
+			if($this->Alert->save($this->data)) {
+				$data['success'] = true;
+				$data['message'] = 'Alert added successfully';				
+			}
+			else {
+				$data['success'] = false;
+				$data['message'] = 'Unable to add alert, please try again.';					
+			}
+			$this->set(compact('data'));
+			$this->render(null, null, '/elements/ajaxreturn');
 		}
-		$users = $this->Alert->User->find('list');
-		$this->set(compact('users'));
 	}
 
 	function admin_edit($id = null) {
