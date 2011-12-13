463d7aac19ecd9804b8a95e4535bc8c1387b5767
Daniel Nolan <daniel@ctsfla.com>
2011-12-13 15:52:30 -0500

    adding test data to alert fixture
    re #299

--- tests/fixtures/alert_fixture.php
+++ tests/fixtures/alert_fixture.php
@@ -1,33 +1,70 @@
 <?php
-/* Alert Fixture generated on: 2011-12-01 10:45:06 : 1322754306 */
+/* Alert Fixture generated on: 2011-12-13 15:10:55 : 1323807055 */
 class AlertFixture extends CakeTestFixture {
 	var $name = 'Alert';
+	var $import = array('model' => 'Alert');
 
-	var $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'watched_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'title' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'message' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'disabled' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
-	);
 
 	var $records = array(
 		array(
-			'id' => 1,
-			'user_id' => 1,
-			'watched_id' => 1,
-			'type' => 'Lorem ipsum dolor sit amet',
-			'title' => 'Lorem ipsum dolor sit amet',
-			'message' => 'Lorem ipsum dolor sit amet',
+			'id' => '20',
+			'name' => 'Vets',
+			'user_id' => '2',
+			'watched_id' => '10',
+			'type' => 'self_sign',
+			'location_id' => '1',
+			'send_email' => '1',
+			'disabled' => 0,
+			'created' => '2011-12-13 11:54:05',
+			'modified' => '2011-12-13 14:05:06'
+		),
+		array(
+			'id' => '19',
+			'name' => 'Test Freddy Filer Alert',
+			'user_id' => '3',
+			'watched_id' => '22',
+			'type' => 'self_sign',
+			'location_id' => '1',
+			'send_email' => '0',
+			'disabled' => 0,
+			'created' => '2011-12-13 10:58:46',
+			'modified' => '2011-12-13 10:59:10'
+		),
+		array(
+			'id' => '17',
+			'name' => 'Test',
+			'user_id' => '2',
+			'watched_id' => '22',
+			'type' => 'self_sign',
+			'location_id' => '1',
+			'send_email' => '1',
 			'disabled' => 1,
-			'created' => '2011-12-01 10:45:06',
-			'modified' => '2011-12-01 10:45:06'
+			'created' => '2011-12-12 16:11:38',
+			'modified' => '2011-12-13 14:04:52'
+		),
+		array(
+			'id' => '18',
+			'name' => 'veteran alert',
+			'user_id' => '1',
+			'watched_id' => '10',
+			'type' => 'self_sign',
+			'location_id' => '1',
+			'send_email' => '1',
+			'disabled' => 0,
+			'created' => '2011-12-12 16:18:54',
+			'modified' => '2011-12-12 16:19:15'
+		),
+		array(
+			'id' => '22',
+			'name' => 'Appointment with Maurice',
+			'user_id' => '1',
+			'watched_id' => '86',
+			'type' => 'self_sign',
+			'location_id' => '1',
+			'send_email' => '1',
+			'disabled' => 0,
+			'created' => '2011-12-13 12:16:09',
+			'modified' => '2011-12-13 12:16:09'
 		),
 	);
 }
