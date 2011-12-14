5f0b31e1accb8bb41a8f9cb7986c86aecb7498eb
Daniel Nolan <daniel@ctsfla.com>
2011-12-14 10:22:48 -0500

    adding tests for alert model
    re #299

--- tests/cases/models/alert.test.php
+++ tests/cases/models/alert.test.php
@@ -3,16 +3,20 @@
 App::import('Model', 'Alert');
 App::import('Lib', 'AtlasTestCase');
 class AlertTestCase extends AtlasTestCase {
-	var $fixtures = array('app.alert', 'app.user', 'app.role', 'app.location', 'app.ftp_document_scanner', 'app.kiosk', 'app.kiosk_button', 'app.master_kiosk_button', 'app.self_sign_log', 'app.self_sign_log_archive', 'app.kiosk_survey', 'app.kiosk_survey_question', 'app.kiosk_survey_response', 'app.kiosk_survey_response_answer', 'app.kiosks_kiosk_survey', 'app.queued_document', 'app.document_queue_category', 'app.bar_code_definition', 'app.document_filing_category', 'app.watched_filing_cat', 'app.program', 'app.program_field', 'app.program_response', 'app.program_response_doc', 'app.program_email', 'app.program_paper_form', 'app.program_instruction', 'app.self_scan_category', 'app.user_transaction', 'app.filed_document');
 
-	function startTest() {
+	public function startTest() {
 		$this->Alert =& ClassRegistry::init('Alert');
 	}
+	
+	public function testGetSelfSignAlerts() {
+		$result = $this->Alert->getSelfSignAlerts(
+			array('level_1' => '10', 'user_id' => '9', 'location_id' => '1'), 'Test');
+		$this->assertEqual($result[0]['username'], 'dnolan');
+	}
 
-	function endTest() {
+	public function endTest() {
 		unset($this->Alert);
 		ClassRegistry::flush();
 	}
 
 }
-?>
