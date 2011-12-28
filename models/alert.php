c65427048988337a60b306a8d3d5e667abc21089
Daniel Nolan <daniel@ctsfla.com>
2011-12-28 14:58:39 -0500

    adding getSelfScanAlerts method to the alert model
    re #312
    
    time: 0h20m

--- models/alert.php
+++ models/alert.php
@@ -54,6 +54,35 @@ class Alert extends AppModel {
 		else return false;
 	}
 
+	public function getSelfScanAlerts($user, $locationId, $docId) {
+		$alerts = $this->find('all', array(
+			'conditions' => array(
+				'Alert.type' => 'self_scan',
+				'Alert.disabled' => 0,
+				'Alert.watched_id' => $user['User']['id'])));
+		if($alerts && $user) {
+			$data = array();
+			
+			$i = 0;
+			foreach($alerts as $alert) {
+				if($locationId !== $alert['Alert']['location_id']) {
+					continue;
+				}			
+				$data[$i]['username'] = strtolower($alert['User']['windows_username']);
+				$data[$i]['email'] = $alert['User']['email'];
+				$data[$i]['send_email'] = $alert['Alert']['send_email'];
+ 				$data[$i]['title'] = 'Self Scan';
+				$message = $user['User']['firstname'] . ' ' . $user['User']['lastname'];
+				$message .= ' self scanned document id: ' . $docId;
+				$data[$i]['message'] = $message;
+				$data[$i]['url'] = Router::url('/admin/queued_documents', true);							
+				$i++;
+			}
+			return $data;	
+		}
+		else return false;
+	}
+
 	public function getCustomerDetailsAlerts($detail, $user, $kiosk)	{
 		$alerts = $this->find('all', array(
 			'conditions' => array(
