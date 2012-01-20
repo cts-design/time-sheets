8fc3a3603f841ee69778d4ac1f9d5248122b17d3
Daniel Nolan <daniel@ctsfla.com>
2012-01-20 11:58:46 -0500

    adding vtype to not allow to_date to be greater than from_date
    re #321
    time: 1h15m

--- webroot/js/queued_documents/admin_index.js
+++ webroot/js/queued_documents/admin_index.js
@@ -1,3 +1,33 @@
+Ext.apply(Ext.form.field.VTypes, {
+    daterange: function(val, field) {
+        var date = field.parseDate(val);
+
+        if (!date) {
+            return false;
+        }
+        if (field.startDateField && (!this.dateRangeMax || (date.getTime() != this.dateRangeMax.getTime()))) {
+            var start = field.up('form').down('#' + field.startDateField);
+            start.setMaxValue(date);
+            start.validate();
+            this.dateRangeMax = date;
+        }
+        else if (field.endDateField && (!this.dateRangeMin || (date.getTime() != this.dateRangeMin.getTime()))) {
+            var end = field.up('form').down('#' + field.endDateField);
+            end.setMinValue(date);
+            end.validate();
+            this.dateRangeMin = date;
+        }
+        /*
+         * Always return true since we're only using this vtype to set the
+         * min/max allowed values (these are tested for after the vtype test)
+         */
+        return true;
+    },
+
+    daterangeText: 'Start date must be less than end date'
+});
+
+
 
 Ext.define('QueuedDocument', {
 	extend: 'Ext.data.Model',
@@ -249,12 +279,18 @@ Ext.define('Atlas.form.DocQueueFilterPanel', {
 	},{
         xtype: 'datefield',
         fieldLabel: 'From',
+        id: 'fromDate',
         name: 'from_date',
+        vtype: 'daterange',
+        endDateField: 'toDate',
         maxValue: new Date()
     },{
         xtype: 'datefield',
+        id: 'toDate',
         fieldLabel: 'To',
         name: 'to_date',
+        vtype: 'daterange',
+        startDateField: 'fromDate',
         maxValue: new Date()
     },{
     	xtype: 'checkbox',
