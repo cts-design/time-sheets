e45faed2d21b4f8774ad7fd8d108461eb4ff38ae
Daniel Nolan <daniel@ctsfla.com>
2012-01-24 10:35:51 -0500

    adding a admin_get_cats action to return all available self scan cateegories
    re #321
    time: 0h20m

--- controllers/self_scan_categories_controller.php
+++ controllers/self_scan_categories_controller.php
@@ -85,4 +85,31 @@ class SelfScanCategoriesController extends AppController {
 		$this->Session->setFlash(__('Self scan category was not deleted', true), 'flash_failure');
 		$this->redirect(array('action' => 'index'));
 	}
+
+    function admin_get_cats() {
+		if($this->RequestHandler->isAjax()) {
+		    $cats = $this->SelfScanCategory->find('all', array(
+				'fields' => array(
+					'SelfScanCategory.id',
+					'SelfScanCategory.cat_1',
+					'SelfScanCategory.cat_2',
+					'SelfScanCategory.cat_3')));
+			$i = 0;
+			if($cats) {
+				foreach ($cats as $cat) {
+					$data['cats'][$i] = $cat['SelfScanCategory'];
+					$i++;				
+				}				
+			}
+			if(!empty($data['cats'])){
+				$data['success'] = true;
+			}
+			else {
+				$data['success'] = true;
+				$data['cats'] = array();
+			}		
+			$this->set(compact('data'));
+			return $this->render(null, null, '/elements/ajaxreturn');
+		}
+    }
 }
