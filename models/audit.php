46d3d8a2e73505d8187033bca238f4dec34eda1f
Brandon Cordell <brandon@brandoncordell.com>
2012-02-21 11:52:16 -0500

    Added the notEmpty validation code for all fields so the audit model test is now passing
    re #12
    time: 0h15m

--- models/audit.php
+++ models/audit.php
@@ -3,4 +3,25 @@
 class Audit extends AppModel {
     public $name = 'Audit';
     public $displayField = 'name';
+
+    public $validate = array(
+        'name' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'An audit name is required'
+            )
+        ),
+        'start_date' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'A start date is required'
+            )
+        ),
+        'end_date' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'An end date is required'
+            )
+        )
+    );
 }
