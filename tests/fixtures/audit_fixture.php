c7a543ffd552faa1b27079ef082d196fb7176d6e
Brandon Cordell <brandon@brandoncordell.com>
2012-02-21 11:46:39 -0500

    Added an audit fixture
    re #12
    time: 0h5m

---
+++ tests/fixtures/audit_fixture.php
@@ -0,0 +1,28 @@
+<?php
+
+class AuditFixture extends CakeTestFixture {
+	var $name = 'Audit';
+	var $import = array('model' => 'Audit');
+
+	var $records = array(
+		array(
+            'id' => 1,
+            'name' => 'Audit 1',
+            'start_date' => '2012-12-01',
+            'end_date' => '2012-12-31',
+            'deleted' => 0,
+            'created' => '2012-12-01 08:00:00',
+            'modified' => '2012-12-01 08:00:00'
+		),
+		array(
+            'id' => 2,
+            'name' => 'Audit 2 Deleted',
+            'start_date' => '2012-12-10',
+            'end_date' => '2012-12-20',
+            'deleted' => 1,
+            'created' => '2012-12-01 08:00:00',
+            'modified' => '2012-12-01 08:00:00'
+		)
+    );
+}
+
