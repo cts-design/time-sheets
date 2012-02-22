8862aa91ffe025ced4798d7b6386f37e565293c2
Brandon Cordell <brandon@brandoncordell.com>
2012-02-22 11:55:56 -0500

    Added code to Audits#admin_read to make the testAdminRead test pass
    re #12
    time: 0h20m

--- controllers/audits_controller.php
+++ controllers/audits_controller.php
@@ -1,9 +1,27 @@
 <?php
 
+App::import('Vendor', 'chromephp/chromephp');
 class AuditsController extends AppController {
     public $name = 'Audits';
 
     public function beforeFilter() {
         parent::beforeFilter();
     }
+
+    public function admin_read() {
+        $audits = $this->Audit->find('all');
+        $data = array();
+
+        $this->log($audits, 'debug');
+
+        foreach ($audits as $key => $value) {
+            $data['audits'][] = $value['Audit'];
+        }
+
+        $data['success'] = true;
+
+        $this->set('data', $data);
+        return $this->render(null, null, '/elements/ajaxreturn');
+    }
 }
+
