ba20d62a83390a8bed98348b2c4cf342c1ce632c
Brandon Cordell <brandon@brandoncordell.com>
2012-02-22 09:53:54 -0500

    Added an AuditsController test, with a failing ajax test for Audits#admin_read
    re #12
    time: 0h15m

---
+++ tests/cases/controllers/audits_controller.test.php
@@ -0,0 +1,71 @@
+<?php
+
+App::import('Controller', 'Audits');
+App::import('Lib', 'AtlasTestCase');
+class TestAuditsController extends AuditsController {
+	public $autoRender = false;
+
+	public function redirect($url, $status = null, $exit = true) {
+		$this->redirectUrl = $url;
+	}
+}
+
+class AuditsControllerTestCase extends AtlasTestCase {
+		
+	public function startTest() {
+		$this->Audits =& new TestAuditsController();
+		$this->Audits->constructClasses();
+        $this->Audits->params['controller'] = 'audits';
+        $this->Audits->params['pass'] = array();
+        $this->Audits->params['named'] = array();	
+		$this->testController = $this->Audits;	
+	}
+
+	public function testAdminRead() {
+		$this->Audits->Component->initialize($this->Audits);	
+		$this->Audits->Session->write('Auth.User', array(
+	        'id' => 2,
+	        'role_id' => 2,
+	        'username' => 'bcordell',
+	        'location_id' => 1
+	    ));
+
+		$expectedResult = array(
+            'data' => array (
+                'audits'=>array(
+                    array(
+                        'id' => 1,
+                        'name' => 'Audit 1',
+                        'start_date' => '2012-12-01',
+                        'end_date' => '2012-12-31',
+                        'deleted' => 0,
+                        'created' => '2012-12-01 08:00:00',
+                        'modified' => '2012-12-01 08:00:00'
+                    ),
+                    array(
+                        'id' => 2,
+                        'name' => 'Audit 2 Deleted',
+                        'start_date' => '2012-12-10',
+                        'end_date' => '2012-12-20',
+                        'deleted' => 1,
+                        'created' => '2012-12-01 08:00:00',
+                        'modified' => '2012-12-01 08:00:00'
+                    )
+                ),
+                'success' => true
+            )
+        );
+
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+		$result = $this->testAction('/admin/audits/read', array('method' => 'get'));
+		$this->assertEqual($expectedResult, $result);
+	}
+		
+	public function endTest() {
+		Configure::write('debug', 2);
+		unset($_SERVER['HTTP_X_REQUESTED_WITH']);		
+		unset($this->Audits);
+		ClassRegistry::flush();
+	}	
+
+}
