08fb8c4e5d2bf7f73027eed8011ea4700871cb79
Daniel Nolan <daniel@ctsfla.com>
2012-02-27 17:01:14 -0500

    adding get_secure_filing_cats action re #203

--- controllers/secure_categories_controller.php
+++ controllers/secure_categories_controller.php
@@ -10,4 +10,24 @@ class SecureCategoriesController extends AppController {
 		
 	}
 
+	function admin_get_secure_filing_cats() {
+		if($this->RequestHandler->isAjax()) {
+			$this->loadModel('DocumentFilingCategory');
+			$cats = $this->DocumentFilingCategory->find('all', 
+				array('conditions' => array('DocumentFilingCategory.secure' => 1)));
+			$data['cats'] = array();
+			if($cats) {
+				$i = 0;
+				foreach($cats as $cat) {
+					$data['cats'][$i]['id'] = $cat['DocumentFilingCategory']['id'];
+					$data['cats'][$i]['name'] = $cat['DocumentFilingCategory']['name'];
+					$data['cats'][$i]['user'] = $cat['DocumentFilingCategory']['users'];
+					$i++;
+				}
+			}
+			$this->set(compact('data'));
+			$this->render(null, null, '/elements/ajaxreturn');
+		}
+	}
+
 }
