4d9b2c7dd676ebcb9d002ff6f47cbb6404519fd1
Daniel Nolan <daniel@ctsfla.com>
2012-02-27 17:03:16 -0500

    starting to build out interface to manage secure filing categories re #203 time: 4h3m

--- webroot/js/secure_categories/admin_index.js
+++ webroot/js/secure_categories/admin_index.js
@@ -1,9 +1,90 @@
-
 Ext.define('SecureFilingCategory', {
 	extend: 'Ext.data.Model',
 	fields: ['id', 'name', 'users']
 });
 
+Ext.create('Ext.data.Store', {
+	storeId: 'SecureFilingCategories',
+	model: 'SecureFilingCategory',
+	proxy: {
+		type: 'ajax',
+		url: '/admin/secure_categories/get_secure_filing_cats',
+		reader: {
+			type: 'json',
+			root: 'cats'
+		},
+		limitParam: undefined,
+		pageParam: undefined,
+		startParam: undefined
+	},
+	autoLoad: true
+});
+
+Ext.define('Atlas.grid.SecureFilingCategoriesPanel', {
+	extend: 'Ext.grid.Panel',
+	alias: 'widget.securefilingcategoriesgridpanel',
+	store: 'SecureFilingCategories',
+	columns: [{
+		text: 'Id',
+		dataIndex: 'id'
+	},{
+		text: 'Name',
+		dataIndex: 'name'
+	}],
+	listeners: {
+		selectionchange: function(selectionModel, selected) {
+			if(selected.length) {
+				var filingCatInfo = Ext.getCmp('filingCatInfo');
+				filingCatTpl.overwrite(filingCatInfo.body, selected[0].data);
+			}
+		}
+	}
+});
+
+var catInfoTplMarkup = '<p><strong>Id:</strong> {id} / <strong>Category Name:</strong> {name} </p>';
+var filingCatTpl = Ext.create('Ext.Template', catInfoTplMarkup);
+
+Ext.define('Atlas.form.SecureFilingCategoryPanel', {
+	extend: 'Ext.form.Panel',
+	alias: 'widget.securefilingcategoryformpanel',
+	items: [{
+		xtype: 'hidden',
+		name: 'id'
+	},{
+		xtype: 'panel',
+		id: 'filingCatInfo',
+		border: 0,
+		data: {},
+		tpl: filingCatTpl,
+		margin: '0 0 10 0'
+	},{
+		xtype: 'boxselect',
+		name: 'users',
+		fieldLabel: 'Allowed Admins'
+	}],
+	buttonAlign: 'left',
+	buttons: [{
+		text: 'Update'
+	}]
+});
+
+
+Ext.define('SecureQueueCategory', {
+	extend: 'Ext.data.Model',
+	fields: ['id', 'name', 'users']
+});
+
+Ext.define('Atlas.grid.SecureQueueCategoriesPanel', {
+	extend: 'Ext.grid.Panel',
+	alias: 'widget.securequeuecategoriesgridpanel',
+	columns: [{
+		text: 'Id',
+		dataIndex: 'id'
+	},{
+		text: 'Name',
+		dataIndex: 'name'
+	}]
+});
 
 Ext.onReady(function(){
 
@@ -18,13 +99,27 @@ Ext.onReady(function(){
 		},
 		items: [{
 			xtype: 'panel',
+			layout: 'vbox',
+			align: 'left',
 			flex: 1,
-			html: 'Filing Categories'
+			border: 0,
+			items: [{
+				xtype: 'securefilingcategoriesgridpanel',
+				title: 'Filing Categories',
+				flex: 2,
+				width: 475
+			},{
+				xtype: 'securefilingcategoryformpanel',
+				width: 475,
+				flex: 1,
+				border: 0,
+				padding: 10
+			}]
 		},{
-			xtype: 'panel',
+			xtype: 'securequeuecategoriesgridpanel',
+			align: 'right',
 			flex: 1,
-			html: 'Queue Categories'
+			title: 'Queue Categories'
 		}]
 	});
 });
-	
