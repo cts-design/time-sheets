6fc2317d688358d8e9ae7c8e47c6d91e46ee563f
Daniel Nolan <daniel@ctsfla.com>
2012-02-28 16:52:16 -0500

    adding admin_update_filing_cat action re #203

--- controllers/secure_categories_controller.php
+++ controllers/secure_categories_controller.php
@@ -1,16 +1,16 @@
 <?php
 class SecureCategoriesController extends AppController {
 
-	var $name = 'SecureCategories';
+	public $name = 'SecureCategories';
 	
-	var $uses = array();
+	public $uses = array();
 	
 
-	function admin_index() {
+	public function admin_index() {
 		
 	}
 
-	function admin_get_secure_filing_cats() {
+	public function admin_get_secure_filing_cats() {
 		if($this->RequestHandler->isAjax()) {
 			$this->loadModel('DocumentFilingCategory');
 			$cats = $this->DocumentFilingCategory->find('all', 
@@ -20,10 +20,45 @@ class SecureCategoriesController extends AppController {
 				$i = 0;
 				foreach($cats as $cat) {
 					$data['cats'][$i]['id'] = $cat['DocumentFilingCategory']['id'];
-					$data['cats'][$i]['name'] = $cat['DocumentFilingCategory']['name'];
-					$data['cats'][$i]['user'] = $cat['DocumentFilingCategory']['users'];
+					$data['cats'][$i]['category'] = '';
+					$data['cats'][$i]['secure_admins'] = json_decode($cat['DocumentFilingCategory']['secure_admins']);
+					$parents = $this->DocumentFilingCategory->getpath(
+						$cat['DocumentFilingCategory']['id'], $fields = array('name'));
+					if($parents) {
+						$ii = 1;
+						foreach ($parents as $parent) {
+							$data['cats'][$i]['category'] .= $parent['DocumentFilingCategory']['name'];
+							if($ii < count($parents)) {
+								$data['cats'][$i]['category'] .= ' - ';
+							}
+							$ii++;
+						}
+					}
 					$i++;
 				}
+			}
+			$this->set(compact('data'));
+			$this->render(null, null, '/elements/ajaxreturn');
+		}
+	}
+
+	public function admin_update_filing_cat() {
+		if($this->RequestHandler->isAjax()) {
+			$this->loadModel('DocumentFilingCategory');
+			if(!empty($this->params['form']['id'])) {
+				$this->data['DocumentFilingCategory'] = $this->params['form'];		
+				if($this->DocumentFilingCategory->save($this->data)) {
+					$data['success'] = true;
+					$data['message'] = 'Filing category updated successfully';
+				}
+				else {
+					$data['success'] = false;
+					$data['message'] = 'Unable to update filing category.';
+				}
+			}
+			else {
+				$data['success'] = false;
+				$data['message'] = 'An error has occurred';
 			}
 			$this->set(compact('data'));
 			$this->render(null, null, '/elements/ajaxreturn');
