f3156f6d42ec07b3d8473406be7c4f42975bf0c6
Daniel Nolan <daniel@ctsfla.com>
2012-02-28 16:53:10 -0500

    building out secure filing categories interface re #203

--- webroot/js/secure_categories/admin_index.js
+++ webroot/js/secure_categories/admin_index.js
@@ -1,6 +1,6 @@
 Ext.define('SecureFilingCategory', {
 	extend: 'Ext.data.Model',
-	fields: ['id', 'name', 'users']
+	fields: ['id', 'category', 'secure_admins']
 });
 
 Ext.create('Ext.data.Store', {
@@ -25,6 +25,22 @@ Ext.define('Admin', {
 	fields: ['id', 'name']
 });
 
+Ext.create('Ext.data.Store', {
+	storeId: 'Admins',
+	model: 'Admin',
+	proxy: {
+		type: 'ajax',
+		url: '/admin/users/get_all_admins',
+		reader: {
+			type: 'json',
+			root: 'admins'
+		},
+		limitParam: undefined,
+		pageParam: undefined,
+		startParam: undefined
+	},
+	autoLoad: true
+});
 
 Ext.define('Atlas.grid.SecureFilingCategoriesPanel', {
 	extend: 'Ext.grid.Panel',
@@ -32,22 +48,28 @@ Ext.define('Atlas.grid.SecureFilingCategoriesPanel', {
 	store: 'SecureFilingCategories',
 	columns: [{
 		text: 'Id',
-		dataIndex: 'id'
+		dataIndex: 'id',
+		hidden: true
 	},{
-		text: 'Name',
-		dataIndex: 'name'
+		text: 'Category',
+		dataIndex: 'category',
+		flex: 1
 	}],
 	listeners: {
 		selectionchange: function(selectionModel, selected) {
 			if(selected.length) {
 				var filingCatInfo = Ext.getCmp('filingCatInfo');
 				filingCatTpl.overwrite(filingCatInfo.body, selected[0].data);
+				Ext.getCmp('secureFilingCategory').loadRecord(selected[0]);
 			}
 		}
 	}
 });
 
-var catInfoTplMarkup = '<p><strong>Id:</strong> {id} / <strong>Category Name:</strong> {name} </p>';
+var catInfoTplMarkup = [
+	'<p style="margin-bottom: 20px;"> Choose a category above view/add/remove allowed admins.</p>',
+	'<p>Category: <span style="margin-left: 10px;">{category}</span></p>'
+];
 var filingCatTpl = Ext.create('Ext.Template', catInfoTplMarkup);
 
 Ext.define('Atlas.form.SecureFilingCategoryPanel', {
@@ -60,17 +82,38 @@ Ext.define('Atlas.form.SecureFilingCategoryPanel', {
 		xtype: 'panel',
 		id: 'filingCatInfo',
 		border: 0,
+		height: 50,
 		data: {},
 		tpl: filingCatTpl,
 		margin: '0 0 10 0'
 	},{
 		xtype: 'boxselect',
-		name: 'users',
+		id: 'adminSelect',
+		encodeSubmitValue: true,
+		name: 'secure_admins',
+		store: 'Admins',
+		displayField: 'name',
+		valueField: 'id',
+		queryMode: 'local',
+		width: 450,
 		fieldLabel: 'Allowed Admins'
 	}],
 	buttonAlign: 'left',
 	buttons: [{
-		text: 'Update'
+		text: 'Update',
+		handler: function() {
+			this.up('form').getForm().submit({
+				waitMsg: 'Please Wait',
+				waitTitle: 'Updating',
+				success: function(form, action) {
+					Ext.Msg.alert('Success', action.result.message);
+					Ext.data.StoreManager.lookup('SecureFilingCategories').load();
+				},
+				failure: function(form, action) {
+					Ext.Msg.alert('Failure', action.result.message);
+				}
+			});
+		}
 	}]
 });
 
@@ -96,8 +139,8 @@ Ext.onReady(function(){
 
 	Ext.create('Ext.Panel', {
 		width: 950,
-		height: 400,
-		title: "Secure Categories",
+		height: 500,
+		border: 0,
 		renderTo: 'secureCategories',
 		layout: {
 			type: 'hbox',
@@ -108,16 +151,17 @@ Ext.onReady(function(){
 			layout: 'vbox',
 			align: 'left',
 			flex: 1,
-			border: 0,
 			items: [{
 				xtype: 'securefilingcategoriesgridpanel',
 				title: 'Filing Categories',
-				flex: 2,
+				flex: 1,
 				width: 475
 			},{
 				xtype: 'securefilingcategoryformpanel',
+				id: 'secureFilingCategory',
+				url: '/admin/secure_categories/update_filing_cat',
 				width: 475,
-				flex: 1,
+				flex: 2,
 				border: 0,
 				padding: 10
 			}]
