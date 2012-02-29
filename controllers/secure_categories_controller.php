05ad533ed99d19999985d7f7b7c38cb922297fc9
Daniel Nolan <daniel@ctsfla.com>
2012-02-29 16:59:49 -0500

    adding admin_get_secure_queue_cats and admin_update_queue_cat actions re #203

--- controllers/secure_categories_controller.php
+++ controllers/secure_categories_controller.php
@@ -42,6 +42,33 @@ class SecureCategoriesController extends AppController {
 		}
 	}
 
+	public function admin_get_secure_queue_cats() {
+		if($this->RequestHandler->isAjax()) {
+			$this->loadModel('DocumentQueueCategory');
+			$cats = $this->DocumentQueueCategory->find('all', array(
+				'conditions' => array('DocumentQueueCategory.secure' => 1),
+				'order' => 'DocumentQueueCategory.name ASC',
+				'fields' => array(
+					'DocumentQueueCategory.name', 
+					'DocumentQueueCategory.id', 
+					'DocumentQueueCategory.secure_admins'
+				)
+			));
+			$data['cats'] = array();
+			if($cats) {
+				$i = 0;
+				foreach($cats as $cat) {
+					$data['cats'][$i]['id'] = $cat['DocumentQueueCategory']['id'];
+					$data['cats'][$i]['category'] = $cat['DocumentQueueCategory']['name'];
+					$data['cats'][$i]['secure_admins'] = json_decode($cat['DocumentQueueCategory']['secure_admins']);
+					$i++;
+				}
+			}
+			$this->set(compact('data'));
+			$this->render(null, null, '/elements/ajaxreturn');
+		}
+	}
+
 	public function admin_update_filing_cat() {
 		if($this->RequestHandler->isAjax()) {
 			$this->loadModel('DocumentFilingCategory');
@@ -64,5 +91,28 @@ class SecureCategoriesController extends AppController {
 			$this->render(null, null, '/elements/ajaxreturn');
 		}
 	}
+
+	public function admin_update_queue_cat() {
+		if($this->RequestHandler->isAjax()) {
+			$this->loadModel('DocumentQueueCategory');
+			if(!empty($this->params['form']['id'])) {
+				$this->data['DocumentQueueCategory'] = $this->params['form'];		
+				if($this->DocumentQueueCategory->save($this->data)) {
+					$data['success'] = true;
+					$data['message'] = 'Queue category updated successfully';
+				}
+				else {
+					$data['success'] = false;
+					$data['message'] = 'Unable to update queue category.';
+				}
+			}
+			else {
+				$data['success'] = false;
+				$data['message'] = 'An error has occurred';
+			}
+			$this->set(compact('data'));
+			$this->render(null, null, '/elements/ajaxreturn');
+		}
+	}	
 
 }
