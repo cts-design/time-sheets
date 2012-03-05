062fb5cab755fe9bae71fe7c4f482227c6c191bd
Daniel Nolan <daniel@ctsfla.com>
2012-03-05 15:14:45 -0500

    fixing formatting

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,10 +57,10 @@ task :cc do
 end
 
 task :chipola do
-	set :deploy_to, "/var/www/vhosts/atlas.onestopahead.com/#{application}"
+  set :deploy_to, "/var/www/vhosts/atlas.onestopahead.com/#{application}"
   set :server_name, 'chipola production'
-	set :user, 'ola_chip0'
-	server "69.68.156.141", :app, :web, :db, :primary => true
+  set :user, 'ola_chip0'
+  server "69.68.156.141", :app, :web, :db, :primary => true
 end
 
 task :clm do
@@ -92,39 +92,39 @@ set :shared_children,       %w(config system tmp tmp/fdf webroot/files/public we
                                storage/thumbnails storage/program_forms storage/program_media)
 
 namespace :deploy do
-	desc "Updates symlinks needed to make application work"
-    task :symlink, :except => { :no_release => true } do
-      on_rollback do
-        if previous_release
-          run "rm -f #{current_path}; ln -s #{previous_release} #{current_path}; true"
-        else
-          logger.important "no previous release to rollback to, rollback of symlink skipped"
-        end
+  desc "Updates symlinks needed to make application work"
+  task :symlink, :except => { :no_release => true } do
+    on_rollback do
+      if previous_release
+        run "rm -f #{current_path}; ln -s #{previous_release} #{current_path}; true"
+      else
+        logger.important "no previous release to rollback to, rollback of symlink skipped"
       end
-      run "ln -s #{shared_path}/system #{latest_release}/webroot/system && ln -s #{shared_path}/tmp #{latest_release}/tmp";
-      run "ln -s #{shared_path}/storage #{latest_release}/storage"
-      run "ln -s #{shared_path}/webroot/files/public #{latest_release}/webroot/files/public"
-      run "ln -s #{shared_path}/webroot/img/public #{latest_release}/webroot/img/public"
-      if (remote_file_exists?("#{shared_path}/webroot/img/default/default_header_logo.jpg"))
-        run "ln -s #{shared_path}/webroot/img/default/default_header_logo.jpg #{latest_release}/webroot/img/default/default_header_logo.jpg"
-      end 
-      run "ln -s #{shared_path}/webroot/img/admin/admin_header_logo.jpg #{latest_release}/webroot/img/admin/admin_header_logo.jpg"
-      run "ln -s #{shared_path}/webroot/img/kiosk/kiosk_header.jpg #{latest_release}/webroot/img/kiosk/kiosk_header.jpg"
-      run "ln -s #{shared_path}/config/core.php #{latest_release}/config/core.php"
-      run "ln -s #{shared_path}/config/atlas.php #{latest_release}/config/atlas.php"
-      run "ln -s #{shared_path}/webroot/index.php #{latest_release}/webroot/index.php"
-      run "ln -s #{shared_path}/webroot/test.php #{latest_release}/webroot/test.php"
-      run "ln -s #{shared_path}/webroot/js/ckfinder/config.php #{latest_release}/webroot/js/ckfinder/config.php"
-      run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}" 
-      cake.database.symlink if (remote_file_exists?(database_path))   
-    end  
+    end
+    run "ln -s #{shared_path}/system #{latest_release}/webroot/system && ln -s #{shared_path}/tmp #{latest_release}/tmp";
+    run "ln -s #{shared_path}/storage #{latest_release}/storage"
+    run "ln -s #{shared_path}/webroot/files/public #{latest_release}/webroot/files/public"
+    run "ln -s #{shared_path}/webroot/img/public #{latest_release}/webroot/img/public"
+    if (remote_file_exists?("#{shared_path}/webroot/img/default/default_header_logo.jpg"))
+      run "ln -s #{shared_path}/webroot/img/default/default_header_logo.jpg #{latest_release}/webroot/img/default/default_header_logo.jpg"
+    end 
+    run "ln -s #{shared_path}/webroot/img/admin/admin_header_logo.jpg #{latest_release}/webroot/img/admin/admin_header_logo.jpg"
+    run "ln -s #{shared_path}/webroot/img/kiosk/kiosk_header.jpg #{latest_release}/webroot/img/kiosk/kiosk_header.jpg"
+    run "ln -s #{shared_path}/config/core.php #{latest_release}/config/core.php"
+    run "ln -s #{shared_path}/config/atlas.php #{latest_release}/config/atlas.php"
+    run "ln -s #{shared_path}/webroot/index.php #{latest_release}/webroot/index.php"
+    run "ln -s #{shared_path}/webroot/test.php #{latest_release}/webroot/test.php"
+    run "ln -s #{shared_path}/webroot/js/ckfinder/config.php #{latest_release}/webroot/js/ckfinder/config.php"
+    run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}" 
+    cake.database.symlink if (remote_file_exists?(database_path))   
+  end  
 end
 
 namespace :cake do
   namespace :schema do
     desc "Update database schema create tables"
-    	task :create, roles => [:web] do
-    	run "cd #{current_release} && cake schema create atlas < #{current_release}/config/schema_create_prompt.txt"
+  	task :create, roles => [:web] do
+      run "cd #{current_release} && cake schema create atlas < #{current_release}/config/schema_create_prompt.txt"
     end
     
     desc "Update database schema update tables"
