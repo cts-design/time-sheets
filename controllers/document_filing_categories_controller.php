19b4da6c41162e590578995a80406650d07e0992
Daniel Nolan <daniel@ctsfla.com>
2012-03-13 10:58:49 -0400

    adding admin_toggle_secure action. adding secure filed to the data array in the admin_index action re #203

--- controllers/document_filing_categories_controller.php
+++ controllers/document_filing_categories_controller.php
@@ -39,7 +39,8 @@ class DocumentFilingCategoriesController extends AppController {
 				        "id" => $node['DocumentFilingCategory']['id'],
 				        "disabled" =>  $disabled,
 				        "cls" => "folder",
-				        "leaf" => ($node['DocumentFilingCategory']['lft'] + 1 == $node['DocumentFilingCategory']['rght'])
+				        "leaf" => ($node['DocumentFilingCategory']['lft'] + 1 == $node['DocumentFilingCategory']['rght']),
+				        "secure" => intval($node['DocumentFilingCategory']['secure'])
 				    );
 				}			 	
 			 }
@@ -228,6 +229,31 @@ class DocumentFilingCategoriesController extends AppController {
 			}
 			$this->set(compact('data'));
 			return $this->render(null, null, '/elements/ajaxreturn');
+		}
+	}
+
+	function admin_toggle_secure() {
+		if($this->RequestHandler->isAjax()) {
+			if(!empty($this->data)) {
+				if(!$this->data['DocumentFilingCategory']['secure']) {
+					$this->data['DocumentFilingCategory']['secure_admins'] = '[]';
+				} 
+				if($this->DocumentFilingCategory->save($this->data)){
+					$data['success'] = true;
+					if($this->data['DocumentFilingCategory']['secure'] == 1){
+						$data['message'] = 'Category secured successfully.';
+						$data['secure'] = true;
+					}
+					elseif($this->data['DocumentFilingCategory']['secure'] == 0) {
+						$data['message'] = 'Category unsecured successfully.';
+						$data['disabled'] = false;						
+					}
+				}
+				else $data['success'] = false;
+			}
+			else $data['success'] = false;
+			$this->set(compact(('data')));
+			$this->render(null, null, '/elements/ajaxreturn');
 		}
 	}
 	
