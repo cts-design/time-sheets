b55e5ad4210c473912d8b628d380fd3c1faa8b48
Brandon Cordell <brandon@brandoncordell.com>
2012-04-16 10:56:20 -0400

    Retabbed and stripped whitespace from programs/admin_index.js
    re #365

--- webroot/js/programs/admin_index.js
+++ webroot/js/programs/admin_index.js
@@ -2,51 +2,51 @@
  * @author dnolan
  */
 
-Ext.onReady(function(){  
-	
-	Ext.QuickTips.init();
-	
-	Ext.define('Program', {
-		extend: 'Ext.data.Model',
-		idProperty: 'id',
-		fields: ['id', 'name', 'actions']
-	});
-	
-	var programStore = Ext.create('Ext.data.Store', {	
-		storeId: 'programStore',
-		autoLoad: true,
-		model: 'Program',
-		proxy: {
-			type: 'ajax',
-			url: '/admin/programs/index',
-			reader: {
-				type: 'json',
-				root: 'programs'
-			}
-		}	
-	});
-	
-	var programGrid = Ext.create('Ext.grid.Panel', {
-		store: programStore,
-		renderTo: 'programGrid',
-		height: 300,
-		title: 'Programs',
-		width: 600,
-		frame: true,
-		columns: [{
-			id: 'id',
-			text: 'Id',
-			dataIndex: 'id',
-			width: 50
-	
-		},{
-			text: 'Program Name',
-			dataIndex: 'name',
-			width: 220
-		},{
-			text: 'Actions',
-			dataIndex: 'actions',
-			width: 300
-		}]
-	});
+Ext.onReady(function(){
+
+  Ext.QuickTips.init();
+
+  Ext.define('Program', {
+    extend: 'Ext.data.Model',
+    idProperty: 'id',
+    fields: ['id', 'name', 'actions']
+  });
+
+  var programStore = Ext.create('Ext.data.Store', {
+    storeId: 'programStore',
+    autoLoad: true,
+    model: 'Program',
+    proxy: {
+      type: 'ajax',
+      url: '/admin/programs/index',
+      reader: {
+        type: 'json',
+        root: 'programs'
+      }
+    }
+  });
+
+  var programGrid = Ext.create('Ext.grid.Panel', {
+    store: programStore,
+    renderTo: 'programGrid',
+    height: 300,
+    title: 'Programs',
+    width: 600,
+    frame: true,
+    columns: [{
+      id: 'id',
+      text: 'Id',
+      dataIndex: 'id',
+      width: 50
+
+    },{
+      text: 'Program Name',
+      dataIndex: 'name',
+      width: 220
+    },{
+      text: 'Actions',
+      dataIndex: 'actions',
+      width: 300
+    }]
+  });
 });
