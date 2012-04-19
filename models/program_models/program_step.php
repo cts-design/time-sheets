e3f5542982f889e3bf3d84111ca83a23e5dd5e38
Daniel Nolan <daniel@ctsfla.com>
2012-04-19 11:07:07 -0400

    adding program_step has one program_instruction relationship

--- models/program_models/program_step.php
+++ models/program_models/program_step.php
@@ -1,34 +1,39 @@
 <?php
 class ProgramStep extends AppModel {
-	var $name = 'ProgramStep';
+    var $name = 'ProgramStep';
     var $actsAs = array('Tree');
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
-	var $belongsTo = array(
-		'Program' => array(
-			'className' => 'Program',
-			'foreignKey' => 'program_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+    var $belongsTo = array(
+        'Program' => array(
+            'className' => 'Program',
+            'foreignKey' => 'program_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 
-	var $hasMany = array(
-		'ProgramFormField' => array(
-			'className' => 'ProgramFormField',
-			'foreignKey' => 'program_step_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+    var $hasOne = array(
+        'ProgramInstruction' => array(
+            'className' => 'ProgramInstruction',
+            'foreignKey' => 'program_step_id'
+        )
+    );
 
+    var $hasMany = array(
+        'ProgramFormField' => array(
+            'className' => 'ProgramFormField',
+            'foreignKey' => 'program_step_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        )
+    );
 }
-?>
