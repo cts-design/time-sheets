e1578fa5411e73006a3869c91f36dd447c72af5c
Daniel Nolan <daniel@ctsfla.com>
2012-04-19 11:09:49 -0400

    removing whitespce and retabbed file

--- models/program_models/program_response.php
+++ models/program_models/program_response.php
@@ -1,41 +1,41 @@
 <?php
 
 class ProgramResponse extends AppModel {
-	
-	var $name = 'ProgramResponse';
-	
-	var $hasMany = array('ProgramResponseDoc', 'ProgramResponseActivity');
-	
-	var $belongsTo = array('Program', 'User');
-	
-	var $validate = array();
+
+    var $name = 'ProgramResponse';
+
+    var $hasMany = array('ProgramResponseDoc', 'ProgramResponseActivity');
+
+    var $belongsTo = array('Program', 'User');
+
+    var $validate = array();
 
     var $actsAs = array('Containable');
-	
-	function getProgramResponse($programId, $userId) {
-		$programResponses = $this->find('all', array(
-			'conditions' => array(
-				'ProgramResponse.user_id' => $userId,
+
+    function getProgramResponse($programId, $userId) {
+        $programResponses = $this->find('all', array(
+            'conditions' => array(
+                'ProgramResponse.user_id' => $userId,
                 'ProgramResponse.program_id' => $programId)));
-		foreach($programResponses as $programResponse) {
-			if($programResponse['ProgramResponse']['complete']) {
-				$return = $programResponse;
-				break;
-			}
-			elseif($programResponse['ProgramResponse']['not_approved'] && 
-				$programResponse['ProgramResponse']['allow_new_response'] == 0) {
-					$return = $programResponse;
-					break;				
-			}
-			elseif($programResponse['ProgramResponse']['expires_on'] > date('Y-m-d H:i:s') && 
-				$programResponse['ProgramResponse']['not_approved'] == 0) {
-					$return = $programResponse;
-					break;
-			}
-		}
-		if(empty($return)) {
-			$return = null;
-		}
-		return $return;		
-	}
+        foreach($programResponses as $programResponse) {
+            if($programResponse['ProgramResponse']['complete']) {
+                $return = $programResponse;
+                break;
+            }
+            elseif($programResponse['ProgramResponse']['not_approved'] &&
+                $programResponse['ProgramResponse']['allow_new_response'] == 0) {
+                    $return = $programResponse;
+                    break;
+            }
+            elseif($programResponse['ProgramResponse']['expires_on'] > date('Y-m-d H:i:s') &&
+                $programResponse['ProgramResponse']['not_approved'] == 0) {
+                    $return = $programResponse;
+                    break;
+            }
+        }
+        if(empty($return)) {
+            $return = null;
+        }
+        return $return;
+    }
 }
