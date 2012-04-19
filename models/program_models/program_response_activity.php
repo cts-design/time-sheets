c6c42b09cbe7b5c190437a8ae22c3adc45d45708
Daniel Nolan <daniel@ctsfla.com>
2012-04-19 09:44:49 -0400

    making validation work on the fly again for auto generated forms and making form answers save to program_response_activities table

--- models/program_models/program_response_activity.php
+++ models/program_models/program_response_activity.php
@@ -2,7 +2,7 @@
 class ProgramResponseActivity extends AppModel {
 	var $name = 'ProgramResponseActivity';
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
+    var $validate = array();
 	var $belongsTo = array(
 		'ProgramResponse' => array(
 			'className' => 'ProgramResponse',
