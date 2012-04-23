81a2010f17c241714c5163160de11a4c8e0d24b9
Daniel Nolan <daniel@ctsfla.com>
2012-04-23 14:58:52 -0400

    changing the way responses are loaded since the new programs architecture will only allow for one response per user per program.

--- models/program_models/program_response.php
+++ models/program_models/program_response.php
@@ -13,38 +13,30 @@ class ProgramResponse extends AppModel {
     var $actsAs = array('Containable');
 
     function getProgramResponse($programId, $userId) {
-        $programResponses = $this->find('all', array(
+        $programResponse = $this->find('first', array(
             'conditions' => array(
                 'ProgramResponse.user_id' => $userId,
                 'ProgramResponse.program_id' => $programId)));
-        foreach($programResponses as $programResponse) {
-            if($programResponse['ProgramResponse']['status'] === 'complete') {
-                $return = $programResponse;
-                break;
-            }
-            if($programResponse['ProgramResponse']['status'] === 'pending_approval') {
-                $return = $programResponse;
-                break;
-            }
-            elseif($programResponse['ProgramResponse']['status'] === 'not_approved' &&
-                ! $programResponse['ProgramResponse']['allow_new_response']) {
-                    $return = $programResponse;
-                    break;
-            }
-            elseif($programResponse['ProgramResponse']['expires_on'] > date('Y-m-d H:i:s') &&
-                $programResponse['ProgramResponse']['status'] === 'not_approved') {
-                    $return = $programResponse;
-                    break;
-            }
-            elseif($programResponse['ProgramResponse']['status'] === 'incomplete' &&
-                $programResponse['ProgramResponse']['expires_on'] > date('Y-m-d H:i:s')) {
-                    $return = $programResponse;
-                    break;
-            }
+        if($programResponse['ProgramResponse']['expires_on'] <= date('Y-m-d H:i:s') &&
+            $programResponse['ProgramResponse']['status'] === 'incomplete') {
+                $expiredResponse = $this->expireResponse($programResponse['ProgramResponse']['id']);
+                if($expiredResponse) {
+                    $programResponse = $expiredResponse;
+                }
         }
-        if(empty($return)) {
-            $return = null;
+        return $programResponse;
+    }
+
+    function expireResponse($responseId) {
+        $this->id = $responseId;
+        $this->saveField('status', 'expired');
+        $response = $this->findById($responseId);
+        if($response['ProgramResponse']['status'] === 'expired') {
+            return $response;
         }
-        return $return;
+        else {
+            return false;
+        }
+
     }
 }
