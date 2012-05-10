d8e17e0bf401dbfa029e11ec976cc464e9eea7de
Daniel Nolan <daniel@ctsfla.com>
2012-05-10 11:51:34 -0400

    adding getProgramAndResponse method to return the program and associated data including the program_response for the current logged in user

--- models/program_models/program.php
+++ models/program_models/program.php
@@ -1,15 +1,27 @@
 <?php
 class Program extends AppModel {
-	var $name = 'Program';
-	var $displayField = 'name';
-    var $actsAs = array('Containable');
-	
-	var $hasMany = array(
-		'ProgramStep',
-		'ProgramResponse',
-		'ProgramEmail',
-		'ProgramDocument',
-		'WatchedFilingCat',
-		'ProgramInstruction'
-		);
+
+	public $name = 'Program';
+	public $displayField = 'name';
+    public $actsAs = array('Containable');
+	public $hasMany = array(
+						'ProgramStep',
+						'ProgramResponse',
+						'ProgramEmail',
+						'ProgramDocument',
+						'WatchedFilingCat',
+						'ProgramInstruction');
+
+	public function getProgramAndResponse($programId, $userId) {
+		$this->contain(array(
+			'ProgramStep' => array(
+				'order' => array('ProgramStep.lft ASC'),
+			),
+			'ProgramInstruction',
+			'ProgramResponse' => array(
+				'conditions' => array('ProgramResponse.user_id' => $userId),
+				'ProgramResponseActivity')));
+         return $this->findById($programId);
+	}
 }
+
