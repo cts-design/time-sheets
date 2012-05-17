3dfaab98823e4ee9e41385a01e5c147c84ab5b90
Brandon Cordell <brandon@brandoncordell.com>
2012-05-17 13:15:20 -0400

    Added the ext model for ProgramResponseDoc

---
+++ webroot/js/Atlas/model/ProgramResponseDoc.js
@@ -0,0 +1,41 @@
+Ext.define('Atlas.model.ProgramResponseDoc', {
+  extend: 'Ext.data.Model',
+  fields: [{
+    name: 'id',
+    type: 'int'
+  }, {
+    name: 'cat_id',
+    type: 'int'
+  }, {
+    name: 'program_response_id',
+    type: 'int'
+  }, {
+    name: 'doc_id',
+    type: 'int'
+  }, {
+    name: 'type',
+    type: 'string'
+  }, {
+    name: 'deleted',
+    type: 'boolean'
+  }, {
+    name: 'deleted_reason',
+    type: 'string'
+  }, {
+    name: 'rejected_reason',
+    type: 'string'
+  }, {
+    name: 'created',
+    type: 'date',
+    dateFormat: 'Y-m-d H:i:s'
+  }, {
+    name: 'modified',
+    type: 'date',
+    dateFormat: 'Y-m-d H:i:s'
+  }],
+  associations: [{
+    type: 'belongsTo',
+    model: 'ProgramResponse',
+    name: 'program_responses'
+  }]
+});
