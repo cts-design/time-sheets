7dbf08dbc0d35fcda4665ba03cd8cf55d07107f8
Daniel Nolan <daniel@ctsfla.com>
2012-06-06 15:41:26 -0400

    adding config file for the CakePHP Queue plugin

---
+++ config/queue.php
@@ -0,0 +1,18 @@
+<?php 
+#seconds to sleep() when no executable job is found
+$config['queue']['sleeptime'] = 10;
+
+#Propability in percent of a old job cleanup happening
+$config['queue']['gcprop'] = 10;
+
+#Default timeout after which a job is requeued if the worker doesn’t report back
+$config['queue']['defaultworkertimeout'] = 120;
+
+#Default number of retries if a job fails or times out.
+$config['queue']['defaultworkerretries'] = 4;
+
+#Seconds of runnig time after which the worker will terminate (0 = unlimited)
+$config['queue']['workermaxruntime'] = 0;
+
+#Should a Workerprocess quit when there are no more tasks for it to execute (true = exit, false = keep running)
+$config['queue']['exitwhennothingtodo'] = false;
