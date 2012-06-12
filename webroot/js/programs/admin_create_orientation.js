69595805e6c906d95cd97adc2c1c20bbcc59f514
Brandon Cordell <brandon@brandoncordell.com>
2012-06-12 14:33:07 -0400

    Added complete instructions to the orientation

--- webroot/js/programs/admin_create_orientation.js
+++ webroot/js/programs/admin_create_orientation.js
@@ -308,6 +308,7 @@ Ext.create('Ext.data.Store', {
   data: [
     { program_id: 0, text: 'Default text Main', type: 'main', created: null, modified: null },
     { program_id: 0, text: 'Default text Expired', type: 'expired', created: null, modified: null },
+    { program_id: 0, text: 'Default text Complete', type: 'complete', created: null, modified: null }
   ],
   storeId: 'ProgramInstructionStore',
   model: 'ProgramInstruction',
