e8340c175ce88d1ba57908752b2d9dac1e546750
Brandon Cordell <brandon@brandoncordell.com>
2012-06-12 14:23:30 -0400

    Added the user_acceptance_required column to the programs table

--- config/schema/atlas.php
+++ config/schema/atlas.php
@@ -1,5 +1,5 @@
 <?php 
-/* atlas schema generated on: 2012-06-11 15:16:46 : 1339442206*/
+/* atlas schema generated on: 2012-06-12 14:23:02 : 1339525382*/
 class atlasSchema extends CakeSchema {
 	var $name = 'atlas';
 
@@ -583,6 +583,7 @@ class atlasSchema extends CakeSchema {
 		'queue_category_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
 		'approval_required' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'form_esign_required' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'user_acceptance_required' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'confirmation_id_length' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 2),
 		'response_expires_in' => array('type' => 'integer', 'null' => false, 'default' => NULL),
 		'send_expiring_soon' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 1),
