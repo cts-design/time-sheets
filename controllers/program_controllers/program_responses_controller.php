55c426e5141c240c2080c7a0bc29bf4d0196eeb1
Daniel Nolan <daniel@ctsfla.com>
2012-06-20 13:53:00 -0400

    Testing OnTime Github Integration [otf: 25 wl: .5 hours]

--- controllers/program_controllers/program_responses_controller.php
+++ controllers/program_controllers/program_responses_controller.php
@@ -689,7 +689,6 @@ class ProgramResponsesController extends AppController {
 					$this->data['ProgramResponseActivity'][0]['id'] = $this->params['form']['reset_form'];
 					$this->data['ProgramResponseActivity'][0]['status'] = 'allow_edit';
 				}
-				
 				if($this->ProgramResponse->saveAll($this->data)) {
 					$data['success'] = true;
 					$data['message'] = 'Program response marked not approved.';
