cdec9d44eec2d6a5057fa7631dec59936d171cd4
Brandon Cordell <brandon@brandoncordell.com>
2012-07-03 15:49:28 -0400

    Cleaned up the document upload method in program documents
    Added the WatchedFilingCat logic directly to the ProgramDocuments
    controller
    
    [otf: 12 wl: 30 minutes]

--- controllers/program_controllers/program_documents_controller.php
+++ controllers/program_controllers/program_documents_controller.php
@@ -9,13 +9,32 @@ class ProgramDocumentsController extends AppController {
 	}
 
 	public function admin_create() {
+		$this->loadModel('WatchedFilingCat');
 		$document = json_decode($this->params['form']['program_documents'], true);
 
 		$this->data['ProgramDocument'] = $document;
+
+		if ($document['cat_3']) {
+			$watchedCatId = $document['cat_3'];
+		} else if ($document['cat_2']) {
+			$watchedCatId = $document['cat_2'];
+		} else if ($document['cat_1']) {
+			$watchedCatId = $document['cat_1'];
+		}
+
+		// get name of cat
+
+		$watchedFilingCat['WatchedFilingCat'] = array(
+			'cat_id' => $watchedCatId,
+			'program_id' => $document['program_id'],
+			'name' => 'Cat Name'
+		);
+
 		$programDocument  = $this->ProgramDocument->save($this->data);
 
 		if ($programDocument) {
-			$data['program_documents'] = $programDocument;
+			$this->WatchedFilingCat->save($watchedFilingCat);
+			$data['program_documents'] = $programDocument['ProgramDocument'];
 			$data['success'] = true;
 		} else {
 			$data['success'] = false;
@@ -73,7 +92,7 @@ class ProgramDocumentsController extends AppController {
 		$this->layout = 'ajax';
 		$storagePath = substr(APP, 0, -1) . Configure::read('Program.media.path');
 
-		switch ($_FILES['media']['type']) {
+		switch ($_FILES['document']['type']) {
 			case 'application/pdf':
 				$path = $storagePath;
 				$ext = '.pdf';
@@ -96,7 +115,7 @@ class ProgramDocumentsController extends AppController {
 
 		if (!file_exists($path . $filename)) {
 			$url = $path . $filename;
-			if (!move_uploaded_file($_FILES['media']['tmp_name'], $url)) {
+			if (!move_uploaded_file($_FILES['document']['tmp_name'], $url)) {
 				$data['success'] = false;
 			} else {
 				$data['success'] = true;
