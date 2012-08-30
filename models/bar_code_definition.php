58211b9e9b70cf2f691de5716ea5b57c86576593
Daniel Nolan <daniel@ctsfla.com>
2012-08-30 13:29:06 -0400

    adding barDecode method to allow for documents with barCodes to have the barcode decoded. [otf: 28 wl: 8 hours]

--- models/bar_code_definition.php
+++ models/bar_code_definition.php
@@ -1,15 +1,17 @@
 <?php
 class BarCodeDefinition extends AppModel {
-	var $name = 'BarCodeDefinition';
-	var $displayField = 'name';
 	
-	var $hasMany = array(
+	public $name = 'BarCodeDefinition';
+
+	public $displayField = 'name';
+	
+	public $hasMany = array(
 	    'QueuedDocument' => array(
 			'className' => 'QueuedDocument',
 			'foreignKey' => 'bar_code_definition_id'
 	    ));
 
-	var $belongsTo = array(
+	public $belongsTo = array(
 		'Cat1' => array(
 		    'className' => 'DocumentFilingCategory',
 		    'foreignKey' => 'cat_1'
@@ -27,5 +29,51 @@ class BarCodeDefinition extends AppModel {
 		    'foreignKey' => 'document_queue_category_id'			
 		)	
 	);
+
+	public function barDecode($document) {
+		$jpg = str_replace('.pdf', '.jpg', $document);	
+		// here we use pdftk to make sure document is only one page.	
+		exec('/usr/local/bin/pdftk ' . $document . ' dump_data | grep NumberOfPages', $output, $return);
+		$pages = (int) substr($output[0], 15);
+		unset($output, $return);
+		if($pages === 1) {
+			// convert pdf to jpg using imagemagick so jpg can be read by bar code binary
+			$command = '/usr/local/bin/convert -density 300 ' . $document . ' ' . $jpg;
+			exec($command, $output, $return);
+			if($return === 0) {
+				unset($output, $return);
+				// read the barcode from jpg
+				$command = '/Users/dnolan/Downloads/bardecoder_7_3_1b_osx/bin/bardecode -t code39 ' . $jpg . ' -K A04B4AB79DD4AA7D297C57A0D6646D7D';   
+				exec($command, $output, $return);
+			}
+			else {
+				return false;
+			}
+			if(isset($output[0])) {
+				$decoded = explode('-', $output[0]);
+				$this->recursive = -1;
+				$barCodeDef = $this->find('first', array(
+					'conditions' => array('BarCodeDefinition.number' => $decoded[0]),
+					'fields' => array('id')));
+				if($barCodeDef) {
+					$return = array(
+						'BarCodeDefinition' => $barCodeDef['BarCodeDefinition'], 
+						'User' => array('id' => (int) $decoded[1])
+					);
+					unlink($jpg);
+					return $return;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			// TODO possibly add logic to deal with multiple page pdfs
+			return false;
+		}
+	}
 }
-?>
