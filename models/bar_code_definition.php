beafda1dd643b480622a9409f12f62ac2cfc897b
Daniel Nolan <daniel@ctsfla.com>
2012-08-30 14:00:30 -0400

    adding production paths for pdftk and bardecode and convert

--- models/bar_code_definition.php
+++ models/bar_code_definition.php
@@ -33,17 +33,17 @@ class BarCodeDefinition extends AppModel {
 	public function barDecode($document) {
 		$jpg = str_replace('.pdf', '.jpg', $document);	
 		// here we use pdftk to make sure document is only one page.	
-		exec('/usr/local/bin/pdftk ' . $document . ' dump_data | grep NumberOfPages', $output, $return);
+		exec('pdftk ' . $document . ' dump_data | grep NumberOfPages', $output, $return);
 		$pages = (int) substr($output[0], 15);
 		unset($output, $return);
 		if($pages === 1) {
 			// convert pdf to jpg using imagemagick so jpg can be read by bar code binary
-			$command = '/usr/local/bin/convert -density 300 ' . $document . ' ' . $jpg;
+			$command = '/usr/bin/convert -density 300 ' . $document . ' ' . $jpg;
 			exec($command, $output, $return);
 			if($return === 0) {
 				unset($output, $return);
 				// read the barcode from jpg
-				$command = '/Users/dnolan/Downloads/bardecoder_7_3_1b_osx/bin/bardecode -t code39 ' . $jpg . ' -K A04B4AB79DD4AA7D297C57A0D6646D7D';   
+				$command = '/bin/bardecode -t code39 ' . $jpg . ' -K A04B4AB79DD4AA7D297C57A0D6646D7D';   
 				exec($command, $output, $return);
 			}
 			else {
