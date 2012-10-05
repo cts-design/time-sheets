105ec107d66b230ffba2ca8e08d79d47b99c4c6a
Daniel Nolan <daniel@ctsfla.com>
2012-10-05 16:13:04 -0400

    adding code from old event model to my new event model [otf: 82]

--- models/event.php
+++ models/event.php
@@ -2,5 +2,17 @@
 class Event extends AppModel {
 
 	public $name = 'Event';
+	
+	public $displayField = 'title';
+	
+	public $belongsTo = 'EventCategory';
 
+	public $actsAs = array(
+		'Translatable' => array(
+			'title', 'description'
+		),
+		'AtlasTranslate' => array(
+			'title', 'description'
+		)
+	);
 }
