772a1227e0a98e6bf2d73ba3ea88971ba498cfc6
Daniel Nolan <daniel@ctsfla.com>
2012-11-15 16:23:05 -0500

    removing has_one relationship to the program_document model. adding overridden paginateCount method

--- models/filed_document.php
+++ models/filed_document.php
@@ -47,12 +47,6 @@ class FiledDocument extends AppModel {
 		)
 	);
 
-	var $hasOne = array(
-		'ProgramResponseDoc' => array(
-			'className' => 'ProgramResponseDoc',
-			'foreignKey' => 'doc_id'
-		));
-
 	var $validate = array(
 	    'filename' => array(
 			'notEmpty' => array(
@@ -103,4 +97,19 @@ class FiledDocument extends AppModel {
 	}
 	return false;
     }
+
+
+	/**
+	 * Overridden paginateCount method
+	 */
+	function paginateCount($conditions = null, $recursive = -1, $extra = array()) {
+		if($conditions) {
+			$this->recursive = $recursive;
+		}
+		else {
+			$this->recursive = -1;
+		}
+		$results = $this->find('count', array('conditions' => $conditions));
+		return $results;
+	}
 }
