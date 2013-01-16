d8afd6dd6f8cb456d0dc55268bff468875af5192
Daniel Nolan <daniel@ctsfla.com>
2013-01-16 10:41:34 -0500

    adding events archive view and js file

---
+++ webroot/js/events/admin_archive.js
@@ -0,0 +1,78 @@
+Ext.define('Event', {
+  extend: 'Ext.data.Model',
+  fields: [
+    {name: 'id'},
+    {name: 'event_category_id'},
+    {name: 'category'},
+    {name: 'name'},
+    {name: 'description'},
+    {name: 'location', serverKey: 'location_id'},
+    {name: 'other_location'},
+    {name: 'url'},
+    {name: 'address'},
+    {name: 'scheduled', type: 'date'},
+    {name: 'seats_available'},
+    {name: 'duration'},
+    {name: 'registered'},
+    {name: 'attended'},
+    {name: 'cat_1'},
+    {name: 'cat_2'},
+    {name: 'cat_3'},
+    {name: 'created', type: 'date', dateFormat: 'n/j h:ia'},
+    {name: 'modified', type: 'date', dateFormat: 'n/j h:ia'}
+  ]
+});
+
+Ext.create('Ext.data.Store', {
+  model: 'Event',
+  storeId: 'eventsStore',
+  proxy: {
+    type: 'ajax',
+    url: '/admin/events/archive',
+    reader: {
+      type: 'json',
+      root: 'events'
+    },
+    directionParam: 'direction',
+    simpleSortMode: true
+  },
+  remoteSort: true,
+  autoLoad: true
+});
+
+Ext.onReady(function(){
+  Ext.QuickTips.init();
+  Ext.create('Ext.grid.Panel', {
+    store: Ext.data.StoreManager.lookup('eventsStore'),
+    title: 'Archive',
+    height: 400,
+    renderTo: 'events',
+    columns: [{
+      text: 'Name',
+      dataIndex: 'name',
+      flex: 1
+    },{
+      text: 'Category',
+      dataIndex: 'category',
+      flex: 1
+    },{
+      text: 'Loaction',
+      dataIndex: 'location',
+      flex: 1
+    },{
+      text: 'Scheduled',
+      dataIndex: 'scheduled',
+      xtype: 'datecolumn',
+      format: 'm/d/y h:i a',
+      width: 120
+    },{
+      text: 'Registered',
+      dataIndex: 'registered',
+      width: 75
+    },{
+      text: 'Attended',
+      dataIndex: 'registered',
+      width: 75
+    }]
+  });
+});
