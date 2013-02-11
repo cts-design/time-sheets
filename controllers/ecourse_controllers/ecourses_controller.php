057bb5c641c5910adbab92c3da66ea73cd9a5f6e
Brandon Cordell <brandon@brandoncordell.com>
2013-02-11 13:27:34 -0500

    Modify EcoursesController#admin_index to receive an ajax request with an ecourse type so it only returns a specific type to each grid
    [otf: 195]

--- controllers/ecourse_controllers/ecourses_controller.php
+++ controllers/ecourse_controllers/ecourses_controller.php
@@ -16,7 +16,22 @@ class EcoursesController extends AppController {
 
 	public function admin_index() {
 		if ($this->RequestHandler->isAjax()) {
-			$data['ecourses'] = array();
+			$ecourseType = $this->params['url']['ecourse_type'];
+
+			$ecourses = $this->Ecourse->find('all', array(
+				'conditions' => array(
+					'Ecourse.type' => $ecourseType
+				)
+			));
+
+			if ($ecourses) {
+				foreach ($ecourses as $key => $ecourse) {
+					$data['ecourses'][] = $ecourse['Ecourse'];
+				}
+			} else {
+				$data['ecourses'] = array();
+			}
+
 			$data['success'] = true;
 			$this->set('data', $data);
 			$this->render('/elements/ajaxreturn');
