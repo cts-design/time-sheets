a6880583c2d7c33c686b6aeea4d599e72fc42da0
Daniel Nolan <daniel@ctsfla.com>
2013-02-19 11:33:30 -0500

    moved files to ecourse views deleted webroot/js/ecourses/admin_assign_users.js [otf: 47 wl: 15 minutes]

--- webroot/js/ecourses/admin_assign_users.js
+++
@@ -1,8 +0,0 @@
-Ext.onReady(function() {
-  Ext.create('Ext.panel.Panel', {
-      title: 'Assign Users',
-      width: 400,
-      html: '<p>Customer Search</p>',
-      renderTo: 'users' 
-  });
-});
