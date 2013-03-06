0357b49029846a9acc91fa3e83561bb5bc1c5f7b
Daniel Nolan <daniel@ctsfla.com>
2013-03-06 14:42:26 -0500

    adding containable behavior

--- models/ecourse_models/ecourse.php
+++ models/ecourse_models/ecourse.php
@@ -3,5 +3,7 @@ class Ecourse extends AppModel {
 	public $name = 'Ecourse';
 	public $displayField = 'name';
 
-	public $hasMany = array('EcourseModule', 'EcourseUser');
+	public $actsAs = array('Containable');
+
+	public $hasMany = array('EcourseModule', 'EcourseResponse', 'EcourseUser');
 }
