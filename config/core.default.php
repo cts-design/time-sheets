44dcddd54b3db3ae1cbccfa6562a1a9c32630a00
Daniel Nolan <danielanolan@gmail.com>
2013-03-08 15:11:13 -0500

    Update core.default.php
    adding auditor to the routes. so new deployments have auditor as a defined route

--- config/core.default.php
+++ config/core.default.php
@@ -85,7 +85,7 @@
  *
  * [Note Routing.admin is deprecated in 1.3.  Use Routing.prefixes instead]
  */
-	Configure::write('Routing.prefixes', array('admin', 'kiosk'));
+	Configure::write('Routing.prefixes', array('admin', 'kiosk', 'auditor'));
 
 /**
  * Turn off all caching application-wide.
