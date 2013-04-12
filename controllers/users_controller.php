3a3550cb5f149a5747b5fb96b634e1e97fc42c50
Brandon Cordell <brandon@brandoncordell.com>
2013-04-12 11:13:51 -0400

    Add conditions to the EcourseResponse module for assigned and public ecourses
    So that only reset responses are shown

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -332,6 +332,10 @@ class UsersController extends AppController {
 					'Ecourse' => array(
 						'EcourseModule',
 						'EcourseResponse' => array(
+							'conditions' => array(
+								'EcourseResponse.user_id' => $this->Auth->user('id'),
+								'EcourseResponse.reset' => 0
+							),
 							'EcourseModuleResponse'
 						)
 					)
@@ -922,6 +926,10 @@ class UsersController extends AppController {
 					'Ecourse' => array(
 						'EcourseModule',
 						'EcourseResponse' => array(
+							'conditions' => array(
+								'EcourseResponse.user_id' => $this->Auth->user('id'),
+								'EcourseResponse.reset' => 0
+							),
 							'EcourseModuleResponse'
 						)
 					)
