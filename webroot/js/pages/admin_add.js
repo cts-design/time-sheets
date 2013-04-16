d2e84f5952877273d05ae6eeeb6b4c16d260348c
Brandon Cordell <brandon@brandoncordell.com>
2013-04-16 10:42:27 -0400

    Rename pages/wysiwyg.js to pages/admin_add.html

---
+++ webroot/js/pages/admin_add.js
@@ -0,0 +1,25 @@
+/**
+ * @author Brandon Cordell
+ * @copyright Complete Technology Solutions 2010
+ * @link http://ctsfla.com
+ * @package ATLAS V3
+ */
+$(function() {
+    CKFinder.setupCKEditor(null, '/js/ckfinder/');
+    var editor = CKEDITOR.replace('PageContent', {
+      toolbar: [
+        ['Source'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        '/',
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Anchor','Link','Unlink','Image','Table','HorizontalRule','SpecialChar'],
+        '/',
+        ['Styles','Format','Font','FontSize'],
+        ['TextColor','BGColor'],
+        ['Maximize', 'ShowBlocks']
+      ]
+    });
+});
