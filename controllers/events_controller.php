23ac8992b9651f7b0936a3ef7dea7247ba841e13
Daniel Nolan <daniel@ctsfla.com>
2013-04-26 15:44:18 -0400

    changing the admin_index action to sort records by date ascending so that the upcoming events are at the top of the list [otf: 228 wl: 5 minutes]

--- controllers/events_controller.php
+++ controllers/events_controller.php
@@ -373,7 +373,7 @@ class EventsController extends AppController {
 		if($this->RequestHandler->isAjax()) {
 			$date = date('Y-m-d H:i:s', strtotime('today'));
 			$events = $this->Event->find('all', array(
-				'order' => array('Event.scheduled DESC'),
+				'order' => array('Event.scheduled ASC'),
 				'conditions' => array('Event.scheduled >= ' => $date)));
 			$this->loadModel('Location');
 			$locations = $this->Location->find('list');
