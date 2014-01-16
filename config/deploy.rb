23b4d7e18bcde91ab57bf4f46b42f75a8ae2c94d
Bill Pierson <bill@ctsfla.com>
2014-01-16 14:32:38 -0500

    Added set :design_branch, "chipola" to chipola
    Added set :design_branch, "chipola" to chipola namespace to hopefully keep their design intact after deployment.
    This is due to most recent deployment which message up their design.

--- config/deploy.rb
+++ config/deploy.rb
@@ -174,6 +174,7 @@ end
 
 # 6pm deploys
 task :chipola do
+  set :design_branch, "chipola"
   set :deploy_to, "/var/www/vhosts/atlas.onestopahead.com/#{application}"
   set :server_name, 'chipola production'
   set :user, 'ola_chip0'
