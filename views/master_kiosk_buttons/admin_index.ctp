5dc80ffc777ad67695b7823a71ea5693ec7fdca5
JosephShering <joseph@ctsfla.com>
2014-06-17 15:24:21 -0400

    Added javascript to create a more responsive interface

--- views/master_kiosk_buttons/admin_index.ctp
+++ views/master_kiosk_buttons/admin_index.ctp
@@ -5,8 +5,22 @@ label > span
 }
 .small-header
 {
-	margin:0px 0px 20px 0px;
+	margin:0px 0px 0px 0px;
 	padding:0px;
+}
+.small-panel-padding
+{
+	padding:0px 5px 20px 5px;
+}
+#masterKioskButtonTree
+{
+	font-size:12pt;
+}
+span.ui-icon
+{
+	display:inline-block;
+	position: relative;
+	top:3px;
 }
 </style>
 <script src="/js/jquery.jstree.js"></script>
@@ -19,8 +33,12 @@ label > span
 			<nav class="navbar navbar-default navbar-static-top" role="nagivation" style="padding:0px 10px">
 				<div class="container-fluid">
 
-					<button class="btn btn-default navbar-btn">
+					<button class="btn btn-default navbar-btn create-root-button">
 						Create Master Button
+					</button>
+
+					<button class="btn btn-default navbar-btn edit-button">
+						Edit Master Button
 					</button>
 
 				</div>
@@ -29,21 +47,33 @@ label > span
 	</div>
 	<div class="row">
 		<div class="col-sm-10 col-sm-offset-1 no-pad bleach" style="padding-bottom:10px">
-			<div class="col-sm-6 no-pad bleach">
+			<div class="col-sm-6 bleach">
 
-				<div id="masterKioskButtonTree">
-					<?= $tree->generate($data, array('element' => 'master_kiosk_buttons/master_kiosk_button_tree')) ?>
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h5 class="small-header">
+							All Master Kiosk Buttons
+						</h5>
+					</div>
+					<div class="panel-body small-panel-padding">
+						<div id="masterKioskButtonTree">
+							<?= $tree->generate($data, array('element' => 'master_kiosk_buttons/master_kiosk_button_tree')) ?>
+						</div>
+					</div>
 				</div>
 			</div>
 
 			<div class="col-sm-6 bleach">
-				<h4 class="small-header">
-					Create New Master Button
+				<?= $this->Session->flash() ?>
+
+				<h4 class="small-header" style="margin-bottom:20px">
+					<span class="action">Create</span> Button <span class="button-identify"></span>
 				</h4>
 
 				<?= $this->Form->create('MasterKioskButton', array('action' => 'add')) ?>
-				<?= $this->Form->input('name', array('class' => 'form-control')) ?>
+				<?= $this->Form->input('name', array('class' => 'form-control', 'maxlength' => 29)) ?>
 				<?= $this->Form->input('parent_id', array('type' => 'hidden', 'value' => NULL)) ?>
+				<?= $this->Form->input('id', array('type' => 'hidden', 'value' => NULL)) ?>
 				<button type="submit" class="btn btn-primary btn-lg" style="margin-top:10px">
 					Create
 				</button>
--- webroot/js/master_kiosk_buttons/buttons.tree.js
+++ webroot/js/master_kiosk_buttons/buttons.tree.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
 	var domain = window.location.hostname;
 
-    $("#masterKioskButtonTree").bind("select_node.jstree", function (e, data) {
+	var $tree 				= $("#masterKioskButtonTree");
+	var $parent_id_field 	= $('input[name="data[MasterKioskButton][parent_id]"]');
+	var $action				= $('.action');
+	var $parent_identify 	= $('.button-identify');
+	var $name_field			= $('input[name="data[MasterKioskButton][name]"]');
+	var $id_field 			= $('input[name="data[MasterKioskButton][id]"');
 
-		node = '#' + data.rslt.obj.attr("id");
-		$("#MasterKioskButtonParentId").attr('value', data.rslt.obj.attr("id"));
-		$("#editButton").attr('href', '/admin/master_kiosk_buttons/edit/' + data.rslt.obj.attr("id"));
+	var name 				= '';
+	var id 					= null;
+
+    $tree.bind("select_node.jstree", function (e, data) {
+
+		id = data.rslt.obj.attr("id");
+		var names = data.rslt.obj.text();
+		names = names.split('\t\t');
+
+		name = $.trim(names[0]);
+
+		$parent_id_field.val(id);
+		$parent_identify.text('for "' + name + '"');
+
+		$name_field.val('');
+		$id_field.val('');
+    	$action.text('Add');
 
     }).bind("delete_node.jstree", function (e, data) {
 		window.location = '/admin/master_kiosk_buttons/delete/' + data.rslt.obj.attr("id");
@@ -30,24 +49,50 @@ $(document).ready(function(){
 	},
 	plugins : [ "themes", "html_data", "ui", "cookies", "types" ]
     });
+
+    $('.create-root-button').click(function(){
+    	$parent_identify.text("");
+    	$parent_id_field.val('');
+    	$name_field.focus();
+
+    	$.jstree._reference("#masterKioskButtonTree").deselect_all();
+		$(this).removeClass('ui-state-focus');
+    });
+
+    $('.edit-button').click(function(){
+    	$action.text('Edit');
+    	$parent_identify.text('');
+
+    	$id_field.val(id);
+    	$parent_id_field.val('');
+
+    	$name_field.val(name);
+    	$name_field.focus();
+    	$name_field.select();
+    });
+
     $("#addButton").click(function(){
-	$("#MasterKioskButtonParentId").attr('value', '');
-	$.cookie('master_kiosk_buttons_selected', null, {domain: domain, path: '/'});
-	$.cookie('master_kiosk_buttons_opened', null, {domain: domain, path: '/'});
-	$.jstree._reference("#masterKioskButtonTree").deselect_all();
-	$(this).removeClass('ui-state-focus');
+		$("#MasterKioskButtonParentId").attr('value', '');
+		$.cookie('master_kiosk_buttons_selected', null, {domain: domain, path: '/'});
+		$.cookie('master_kiosk_buttons_opened', null, {domain: domain, path: '/'});
+		$.jstree._reference("#masterKioskButtonTree").deselect_all();
+		$(this).removeClass('ui-state-focus');
     });
+
     $("#deleteButton").click(function(){
-	$.jstree._reference("#masterKioskButtonTree").delete_node(node.toString());
-	$(this).removeClass('ui-state-focus');
-	return false;
+		$.jstree._reference("#masterKioskButtonTree").delete_node(node.toString());
+		$(this).removeClass('ui-state-focus');
+		return false;
     });
+
     $('.expand').toggle(function(){
-	$.jstree._reference("#masterKioskButtonTree").open_all();
-	$(this).html('Collapse All')
-    },
-    function(){
-	$.jstree._reference("#masterKioskButtonTree").close_all();
-	$(this).html('Expand All')
+		$.jstree._reference("#masterKioskButtonTree").open_all();
+		$(this).html('Collapse All')
+    }, function(){
+		$.jstree._reference("#masterKioskButtonTree").close_all();
+		$(this).html('Expand All')
     });
 });
+
+
+
